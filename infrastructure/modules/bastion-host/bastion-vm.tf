d545f8fe050a37aa6bc4eb76d4de631516229986
Lester March <lestermarch@outlook.com>
2022-08-05 10:31:34 +0100

    fix(mgmt): remove hotpatching from bastion vm and disable vm extensions

--- infrastructure/modules/bastion-host/bastion-vm.tf
+++ infrastructure/modules/bastion-host/bastion-vm.tf
@@ -13,19 +13,17 @@ resource "azurerm_network_interface" "jumpbox" {
 }
 
 resource "azurerm_windows_virtual_machine" "jumpbox" {
-  #checkov:skip=CKV_AZURE_50:   SKIP: VM extensions required for hotpatching
   #checkov:skip=CKV_AZURE_151:  TODO: Enable VM encryption
-  name                     = "bas-vm-${local.resource_suffix}"
-  location                 = var.location
-  resource_group_name      = var.resource_group_name
-  size                     = var.bastion_vm_size
-  admin_username           = var.bastion_vm_username
-  admin_password           = random_password.bastion_vm_admin_password.result
-  computer_name            = "bastion-${random_string.unique_id.id}"
-  enable_automatic_updates = true
-  network_interface_ids    = [azurerm_network_interface.jumpbox.id]
-  patch_mode               = "AutomaticByPlatform"
-  hotpatching_enabled      = true
+  name                       = "bas-vm-${local.resource_suffix}"
+  location                   = var.location
+  resource_group_name        = var.resource_group_name
+  size                       = var.bastion_vm_size
+  admin_username             = var.bastion_vm_username
+  admin_password             = random_password.bastion_vm_admin_password.result
+  allow_extension_operations = false
+  computer_name              = "bastion-${random_string.unique_id.id}"
+  network_interface_ids      = [azurerm_network_interface.jumpbox.id]
+  provision_vm_agent         = false
 
   os_disk {
     caching              = "ReadWrite"
