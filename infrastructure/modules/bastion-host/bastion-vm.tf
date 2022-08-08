9a9015f922297f3a1ee0de6ab01fb458798c5be9
Lester March <lestermarch@outlook.com>
2022-08-08 13:39:16 +0100

    fix: remove redundant comments

--- infrastructure/modules/bastion-host/bastion-vm.tf
+++ infrastructure/modules/bastion-host/bastion-vm.tf
@@ -30,13 +30,6 @@ resource "azurerm_windows_virtual_machine" "jumpbox" {
     storage_account_type = "Standard_LRS"
   }
 
-  # source_image_reference {
-  #   publisher = "MicrosoftWindowsServer"
-  #   offer     = "WindowsServer"
-  #   sku       = "2022-datacenter-azure-edition-core"
-  #   version   = "latest"
-  # }
-
   source_image_reference {
     publisher = "MicrosoftWindowsDesktop"
     offer     = "windows-11"
