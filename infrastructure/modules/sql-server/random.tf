53309837392c2fc228c831882de7af6ab36b6f1b
Lester March <lestermarch@outlook.com>
2022-08-11 13:21:27 +0100

    fix: remove redundant resource

--- infrastructure/modules/sql-server/random.tf
+++
@@ -1,9 +0,0 @@
-resource "random_password" "sql_administrator_password" {
-  length           = 32
-  special          = true
-  override_special = "#&-_+"
-  min_lower        = 2
-  min_upper        = 2
-  min_numeric      = 2
-  min_special      = 2
-}
