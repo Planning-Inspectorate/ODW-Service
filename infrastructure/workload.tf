45a757996097071627b739284f09b1d814351c5c
Lester March <lestermarch@outlook.com>
2022-08-11 13:28:12 +0100

    fix: provision sql server to sql server group

--- infrastructure/workload.tf
+++ infrastructure/workload.tf
@@ -130,7 +130,7 @@ module "sql_server" {
   source = "./modules/sql-server"
 
   environment         = var.environment
-  resource_group_name = azurerm_resource_group.monitoring.name
+  resource_group_name = azurerm_resource_group.sql_server[0].name
   location            = module.azure_region.location_cli
   service_name        = local.service_name
 
