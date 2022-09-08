7a8b354127057c018d1632f6771c67431e970298
Lester March <lestermarch@outlook.com>
2022-09-08 09:51:27 +0100

    feat(alerts): add data lake latency metric alert

--- infrastructure/modules/synapse-monitoring/alert-data-lake-metrics.tf
+++ infrastructure/modules/synapse-monitoring/alert-data-lake-metrics.tf
@@ -22,3 +22,26 @@ resource "azurerm_monitor_metric_alert" "data_lake_capacity" {
 
   tags = local.tags
 }
+
+resource "azurerm_monitor_metric_alert" "data_lake_latency" {
+  name                = "Data Lake Latency"
+  resource_group_name = var.resource_group_name
+  scopes              = [var.data_lake_account_id]
+  description         = "Triggers an alert if the Data Lake Gen2 Storage Account exceeds 1ms latency"
+  enabled             = var.alert_group_platform_enabled
+  frequency           = "PT1M"
+  severity            = 2
+  window_size         = "PT5M"
+
+  criteria {
+    metric_name      = "SuccessE2ELatency"
+    metric_namespace = "Microsoft.Storage/storageAccounts"
+    aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 1000
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.platform_alerts.id
+  }
+}
