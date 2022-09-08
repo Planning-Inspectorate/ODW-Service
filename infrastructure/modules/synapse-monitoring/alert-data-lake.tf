8c29706c0de7f74e484bf4f5859880edf71067ec
Lester March <lestermarch@outlook.com>
2022-09-08 09:55:13 +0100

    feat(alerts): combine metric and activity log alert files

--- infrastructure/modules/synapse-monitoring/alert-data-lake.tf
+++ infrastructure/modules/synapse-monitoring/alert-data-lake.tf
@@ -58,3 +58,51 @@ resource "azurerm_monitor_activity_log_alert" "data_lake_resource_health" {
 
   tags = local.tags
 }
+
+resource "azurerm_monitor_metric_alert" "data_lake_capacity" {
+  name                = "Data Lake Capacity"
+  resource_group_name = var.resource_group_name
+  scopes              = [var.data_lake_account_id]
+  description         = "Triggers an alert if the Data Lake Gen2 Storage Account exceeds the specified capacity"
+  enabled             = var.alert_group_platform_enabled
+  frequency           = "PT15M"
+  severity            = 2
+  window_size         = "PT1H"
+
+  criteria {
+    metric_name      = "UsedCapacity"
+    metric_namespace = "Microsoft.Storage/storageAccounts"
+    aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = var.alert_threshold_data_lake_capacity_bytes
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.platform_alerts.id
+  }
+
+  tags = local.tags
+}
+
+resource "azurerm_monitor_metric_alert" "data_lake_latency" {
+  name                = "Data Lake Latency"
+  resource_group_name = var.resource_group_name
+  scopes              = [var.data_lake_account_id]
+  description         = "Triggers an alert if the Data Lake Gen2 Storage Account exceeds 1ms latency"
+  enabled             = var.alert_group_platform_enabled
+  frequency           = "PT1M"
+  severity            = 2
+  window_size         = "PT5M"
+
+  criteria {
+    metric_name      = "SuccessE2ELatency"
+    metric_namespace = "Microsoft.Storage/storageAccounts"
+    aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 1000
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.platform_alerts.id
+  }
+}
--- infrastructure/modules/synapse-monitoring/alert-synapse.tf
+++ infrastructure/modules/synapse-monitoring/alert-synapse.tf
@@ -58,3 +58,34 @@ resource "azurerm_monitor_activity_log_alert" "synapse_workspace_resource_health
 
   tags = local.tags
 }
+
+resource "azurerm_monitor_metric_alert" "synapse_pipeline_runs_failed" {
+  name                = "Synapse Pipeline Runs Failed"
+  resource_group_name = var.resource_group_name
+  scopes              = [var.synapse_workspace_id]
+  description         = "Triggers an alert if any Synapse pipeline runs fail"
+  enabled             = var.alert_group_synapse_enabled
+  frequency           = "PT1M"
+  severity            = 1
+  window_size         = "PT5M"
+
+  criteria {
+    metric_name      = "IntegrationPipelineRunsEnded"
+    metric_namespace = "Microsoft.Synapse/workspaces"
+    aggregation      = "Count"
+    operator         = "GreaterThanOrEqual"
+    threshold        = 1
+
+    dimension {
+      name     = "Result"
+      operator = "Include"
+      values   = ["Failed"]
+    }
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.synapse_alerts.id
+  }
+
+  tags = local.tags
+}
