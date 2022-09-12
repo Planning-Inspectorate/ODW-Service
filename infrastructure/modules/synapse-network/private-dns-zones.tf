354144cec3efa722bffe380b7bb49a8a27a1afc8
Lester March <lestermarch@outlook.com>
2022-09-12 10:07:03 +0100

    fix(dns): specify data lake private dns zone name for vnet link

--- infrastructure/modules/synapse-network/private-dns-zones.tf
+++ infrastructure/modules/synapse-network/private-dns-zones.tf
@@ -15,7 +15,7 @@ resource "azurerm_private_dns_zone" "synapse" {
 resource "azurerm_private_dns_zone_virtual_network_link" "data_lake" {
   name                  = "dfs-${azurerm_virtual_network.synapse.name}"
   resource_group_name   = var.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.synapse.name
+  private_dns_zone_name = azurerm_private_dns_zone.data_lake.name
   virtual_network_id    = azurerm_virtual_network.synapse.id
 
   tags = local.tags
