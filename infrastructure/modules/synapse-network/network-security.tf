e5ed29c72ed84ae90a77784a9f8a39845c8ffe78
Lester March <lestermarch@outlook.com>
2022-10-07 09:53:06 +0100

    test(network): try loop conditional to prevent azure service nsg dynamic creation

--- infrastructure/modules/synapse-network/network-security.tf
+++ infrastructure/modules/synapse-network/network-security.tf
@@ -1,5 +1,5 @@
 resource "azurerm_network_security_group" "nsg" {
-  for_each = { for k, v in azurerm_subnet.synapse : k => v.id }
+  for_each = { for k, v in azurerm_subnet.synapse : k => v.id if !startswith(k, "Azure") }
 
   name                = "pins-nsg-${lower(replace(each.key, "Subnet", ""))}-${local.resource_suffix}"
   location            = var.location
@@ -9,7 +9,7 @@ resource "azurerm_network_security_group" "nsg" {
 }
 
 resource "azurerm_subnet_network_security_group_association" "nsg" {
-  for_each = { for k, v in azurerm_subnet.synapse : k => v.id }
+  for_each = { for k, v in azurerm_subnet.synapse : k => v.id if !startswith(k, "Azure") }
 
   network_security_group_id = "${var.resource_group_id}/${local.nsg_path}/pins-nsg-${lower(replace(each.key, "Subnet", ""))}-${local.resource_suffix}"
   subnet_id                 = each.value
