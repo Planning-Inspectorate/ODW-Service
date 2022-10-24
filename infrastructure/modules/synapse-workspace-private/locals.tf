bd1b7dba2779d768e644e3d55163a4198310a674
Lester March <lestermarch@outlook.com>
2022-10-24 09:07:10 +0100

    feat(firewall): add firewall configuration to synapse workspace

--- infrastructure/modules/synapse-workspace-private/locals.tf
+++ infrastructure/modules/synapse-workspace-private/locals.tf
@@ -2,6 +2,12 @@ locals {
   module_name     = "synapse-workspace-private"
   resource_suffix = "${var.service_name}-${var.environment}-${module.azure_region.location_short}"
 
+  firewall_allowed_ip_addresses = [
+    for address in var.firewall_allowed_ip_addresses : {
+      address = can(split("/", address)[1]) ? address : "${address}/32"
+    }
+  ]
+
   synapse_role_assignments = flatten([
     for role, principals in var.synapse_role_assignments : [
       for principal in principals : {
