f0d55a377ab4c91291a0bc04dc7e2dfd38834b0c
Lester March <lestermarch@outlook.com>
2022-10-26 11:33:36 +0100

    fix(data): use iterator for dynamic attribute referencing

--- infrastructure/modules/synapse-data-lake/data-lake-lifecycle-policy.tf
+++ infrastructure/modules/synapse-data-lake/data-lake-lifecycle-policy.tf
@@ -17,21 +17,21 @@ resource "azurerm_storage_management_policy" "synapse" {
 
       actions {
         base_blob {
-          tier_to_cool_after_days_since_modification_greater_than    = try(rule.value.cool_tier_since_modified_days, null)
-          tier_to_archive_after_days_since_modification_greater_than = try(rule.value.archive_tier_since_modified_days, null)
-          delete_after_days_since_modification_greater_than          = try(rule.value.delete_since_modified_days, null)
+          tier_to_cool_after_days_since_modification_greater_than    = can(rule.value.cool_tier_since_modified_days) ? rule.value.cool_tier_since_modified_days : null
+          tier_to_archive_after_days_since_modification_greater_than = can(rule.value.archive_tier_since_modified_days) ? rule.value.archive_tier_since_modified_days : null
+          delete_after_days_since_modification_greater_than          = can(rule.value.delete_since_modified_days) ? rule.value.delete_since_modified_days : null
         }
 
         snapshot {
-          change_tier_to_cool_after_days_since_creation    = try(rule.value.cool_tier_since_created_days, null)
-          change_tier_to_archive_after_days_since_creation = try(rule.value.archive_tier_since_created_days, null)
-          delete_after_days_since_creation_greater_than    = try(rule.value.delete_since_created_days, null)
+          change_tier_to_cool_after_days_since_creation    = can(rule.value.cool_tier_since_created_days) ? rule.value.cool_tier_since_created_days : null
+          change_tier_to_archive_after_days_since_creation = can(rule.value.archive_tier_since_created_days) ? rule.value.archive_tier_since_created_days : null
+          delete_after_days_since_creation_greater_than    = can(rule.value.delete_since_created_days) ? rule.value.delete_since_created_days : null
         }
 
         version {
-          change_tier_to_cool_after_days_since_creation    = try(rule.value.cool_tier_since_created_days, null)
-          change_tier_to_archive_after_days_since_creation = try(rule.value.archive_tier_since_created_days, null)
-          delete_after_days_since_creation                 = try(rule.value.delete_since_created_days, null)
+          change_tier_to_cool_after_days_since_creation    = can(rule.value.cool_tier_since_created_days) ? rule.value.cool_tier_since_created_days : null
+          change_tier_to_archive_after_days_since_creation = can(rule.value.archive_tier_since_created_days) ? rule.value.archive_tier_since_created_days : null
+          delete_after_days_since_creation                 = can(rule.value.delete_since_created_days) ? rule.value.delete_since_created_days : null
         }
       }
     }
