e89118774073df8dd5444d317bb3b2f75f4d75c0
Lester March <lestermarch@outlook.com>
2022-10-26 16:05:04 +0100

    fix(shir): disable encryption at host as not supported

--- infrastructure/modules/synapse-shir/shir-vm.tf
+++ infrastructure/modules/synapse-shir/shir-vm.tf
@@ -14,17 +14,17 @@ resource "azurerm_network_interface" "shir" {
 
 resource "azurerm_windows_virtual_machine" "synapse" {
   #checkov:skip=CKV_AZURE_50: VM extensions are required for provisioning and updates
-  name                       = "pins-vm-shir-${local.resource_suffix}"
-  resource_group_name        = var.resource_group_name
-  location                   = var.location
-  size                       = var.runtime_vm_size
-  admin_username             = var.runtime_vm_username
-  admin_password             = random_password.shir_vm_administrator_password.result
-  computer_name              = "shir-${random_string.unique_id.id}"
-  encryption_at_host_enabled = true
-  network_interface_ids      = [azurerm_network_interface.shir.id]
-  patch_mode                 = contains(split("_", var.runtime_vm_size), "v2") ? "AutomaticByPlatform" : "AutomaticByOS"
-  hotpatching_enabled        = contains(split("_", var.runtime_vm_size), "v2") ? true : false
+  #checkov:skip=CKV_AZURE_151: Host encryption is not supported
+  name                  = "pins-vm-shir-${local.resource_suffix}"
+  resource_group_name   = var.resource_group_name
+  location              = var.location
+  size                  = var.runtime_vm_size
+  admin_username        = var.runtime_vm_username
+  admin_password        = random_password.shir_vm_administrator_password.result
+  computer_name         = "shir-${random_string.unique_id.id}"
+  network_interface_ids = [azurerm_network_interface.shir.id]
+  patch_mode            = contains(split("_", var.runtime_vm_size), "v2") ? "AutomaticByPlatform" : "AutomaticByOS"
+  hotpatching_enabled   = contains(split("_", var.runtime_vm_size), "v2") ? true : false
 
   os_disk {
     caching              = "ReadWrite"
