4b6eaed3dc108059afdf812f82d17fca90870160
Lester March <lestermarch@outlook.com>
2022-10-27 16:58:28 +0100

    fix(shir): use correct values for extension publisher and type

--- infrastructure/modules/synapse-shir/shir-vm-setup.tf
+++ infrastructure/modules/synapse-shir/shir-vm-setup.tf
@@ -1,9 +1,9 @@
 resource "azurerm_virtual_machine_extension" "custom_script" {
   name                 = "shir-ext-${random_string.unique_id.id}"
   virtual_machine_id   = azurerm_windows_virtual_machine.synapse.id
-  publisher            = "Microsoft.Azure.Extensions"
-  type                 = "CustomScript"
-  type_handler_version = "2.0"
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.10"
 
   settings = <<SETTINGS
   {
