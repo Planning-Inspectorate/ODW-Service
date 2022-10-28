710a2d7e7fa665f29e9f6f34b07c63053897f8a8
Lester March <lestermarch@outlook.com>
2022-10-28 13:16:39 +0100

    test(shir): use public access container and access without sas token

--- infrastructure/modules/synapse-shir/data.tf
+++ infrastructure/modules/synapse-shir/data.tf
@@ -1,36 +1,36 @@
 data "azurerm_client_config" "current" {}
 
-data "azurerm_storage_account_sas" "shir" {
-  connection_string = azurerm_storage_account.shir.primary_connection_string
-  https_only        = true
-  signed_version    = "2021-06-08"
+# data "azurerm_storage_account_sas" "shir" {
+#   connection_string = azurerm_storage_account.shir.primary_connection_string
+#   https_only        = true
+#   signed_version    = "2021-06-08"
 
-  start  = timestamp()
-  expiry = timeadd(timestamp(), "30m")
+#   start  = timestamp()
+#   expiry = timeadd(timestamp(), "30m")
 
-  permissions {
-    read    = true
-    write   = false
-    delete  = false
-    list    = false
-    add     = false
-    create  = false
-    update  = false
-    process = false
-    tag     = false
-    filter  = false
-  }
+#   permissions {
+#     read    = true
+#     write   = false
+#     delete  = false
+#     list    = false
+#     add     = false
+#     create  = false
+#     update  = false
+#     process = false
+#     tag     = false
+#     filter  = false
+#   }
 
-  resource_types {
-    service   = true
-    container = true
-    object    = true
-  }
+#   resource_types {
+#     service   = true
+#     container = true
+#     object    = true
+#   }
 
-  services {
-    blob  = true
-    queue = false
-    table = false
-    file  = false
-  }
-}
+#   services {
+#     blob  = true
+#     queue = false
+#     table = false
+#     file  = false
+#   }
+# }
--- infrastructure/modules/synapse-shir/shir-vm-setup.tf
+++ infrastructure/modules/synapse-shir/shir-vm-setup.tf
@@ -7,11 +7,19 @@ resource "azurerm_virtual_machine_extension" "custom_script" {
 
   protected_settings = <<PROTECTED_SETTINGS
     {
-      "fileUris": ["${azurerm_storage_blob.install_shir.url}${data.azurerm_storage_account_sas.shir.sas}&sr=b"],
+      "fileUris": ["${azurerm_storage_blob.install_shir.url}"],
       "commandToExecute": "powershell.exe Install-Shir.ps1 -authKey ${azurerm_synapse_integration_runtime_self_hosted.synapse.authorization_key_primary}",
       "managedIdentity": {}
     }
 PROTECTED_SETTINGS
+
+  #   protected_settings = <<PROTECTED_SETTINGS
+  #     {
+  #       "fileUris": ["${azurerm_storage_blob.install_shir.url}${data.azurerm_storage_account_sas.shir.sas}&sr=b"],
+  #       "commandToExecute": "powershell.exe Install-Shir.ps1 -authKey ${azurerm_synapse_integration_runtime_self_hosted.synapse.authorization_key_primary}",
+  #       "managedIdentity": {}
+  #     }
+  # PROTECTED_SETTINGS
 
   depends_on = [
     azurerm_private_endpoint.shir_blob,
