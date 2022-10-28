93a670a055364c173cd4aa62a2ba9a239a456777
Lester March <lestermarch@outlook.com>
2022-10-28 14:49:43 +0100

    chore(shir): cleanup comments

--- infrastructure/modules/synapse-shir/shir-vm-setup.tf
+++ infrastructure/modules/synapse-shir/shir-vm-setup.tf
@@ -13,14 +13,6 @@ resource "azurerm_virtual_machine_extension" "custom_script" {
     }
 PROTECTED_SETTINGS
 
-  #   protected_settings = <<PROTECTED_SETTINGS
-  #     {
-  #       "fileUris": ["${azurerm_storage_blob.install_shir.url}${data.azurerm_storage_account_sas.shir.sas}&sr=b"],
-  #       "commandToExecute": "powershell.exe Install-Shir.ps1 -authKey ${azurerm_synapse_integration_runtime_self_hosted.synapse.authorization_key_primary}",
-  #       "managedIdentity": {}
-  #     }
-  # PROTECTED_SETTINGS
-
   depends_on = [
     azurerm_private_endpoint.shir_blob,
     azurerm_role_assignment.shir_vm
