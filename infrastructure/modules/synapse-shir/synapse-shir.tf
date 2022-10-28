bb4e862e9a2e7333c75c6a8256d983c80e2b2ab4
Lester March <lestermarch@outlook.com>
2022-10-28 17:27:27 +0100

    feat(shir): cleanup files after successful provisioning

--- infrastructure/modules/synapse-shir/synapse-shir.tf
+++ infrastructure/modules/synapse-shir/synapse-shir.tf
@@ -1,4 +1,4 @@
 resource "azurerm_synapse_integration_runtime_self_hosted" "synapse" {
-  name                 = "pins-shir"
+  name                 = "PinsIntegrationRuntime"
   synapse_workspace_id = var.synapse_workspace_id
 }
