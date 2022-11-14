2fc3193eb4469da3bd1da40152b5bc74a84b9062
Lester March <lestermarch@outlook.com>
2022-11-14 16:34:13 +0000

    fix(devops): target correct failover resource group for images

--- infrastructure/workload-agent-pool.tf
+++ infrastructure/workload-agent-pool.tf
@@ -1,16 +1,16 @@
 module "devops_agent_pool" {
   source = "./modules/devops-agent-pool"
 
-  environment  = var.environment
-  location     = module.azure_region.location_cli
-  service_name = local.service_name
+  environment         = var.environment
+  resource_group_name = var.devops_agent_pool_resource_group_name
+  location            = module.azure_region.location_cli
+  service_name        = local.service_name
 
   deploy_agent_pool         = var.deploy_agent_pool
   devops_agent_image_prefix = var.devops_agent_image_prefix
   devops_agent_instances    = var.devops_agent_instances
   devops_agent_subnet_name  = local.compute_subnet_name
   devops_agent_vm_sku       = var.devops_agent_vm_sku
-  resource_group_name       = var.devops_agent_pool_resource_group_name
   vnet_subnet_ids           = module.synapse_network.vnet_subnets
 
   depends_on = [
@@ -25,16 +25,16 @@ module "devops_agent_pool_failover" {
 
   source = "./modules/devops-agent-pool"
 
-  environment  = var.environment
-  location     = module.azure_region.location_cli
-  service_name = local.service_name
+  environment         = var.environment
+  resource_group_name = var.devops_agent_pool_resource_group_name_failover
+  location            = module.azure_region.paired_location.location_cli
+  service_name        = local.service_name
 
   deploy_agent_pool         = var.deploy_agent_pool
   devops_agent_image_prefix = var.devops_agent_image_prefix
   devops_agent_instances    = var.devops_agent_instances
   devops_agent_subnet_name  = local.compute_subnet_name
   devops_agent_vm_sku       = var.devops_agent_vm_sku
-  resource_group_name       = var.devops_agent_pool_resource_group_name_failover
   vnet_subnet_ids           = module.synapse_network_failover.vnet_subnets
 
   depends_on = [
