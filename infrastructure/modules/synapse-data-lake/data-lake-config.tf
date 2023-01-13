418cbdfb144c7c8097ea4f0df87a1dec802b8a71
Lester March <lestermarch@outlook.com>
2023-01-13 10:07:34 +0000

    fix(data-lake): update storage account name reference

--- infrastructure/modules/synapse-data-lake/data-lake-config.tf
+++ infrastructure/modules/synapse-data-lake/data-lake-config.tf
@@ -2,7 +2,7 @@ resource "azurerm_storage_blob" "test" {
   for_each = toset(var.data_lake_configuration_files)
 
   name                   = each.key
-  storage_account_name   = azurerm_storage_account.synapse
+  storage_account_name   = azurerm_storage_account.synapse.name
   storage_container_name = "odw-config"
   type                   = "Block"
   source                 = each.key
