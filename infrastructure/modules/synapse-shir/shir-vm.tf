f39fd8623a22a6d8f2eaa9fbd1d7fe7c49e383f9
Lester March <lestermarch@outlook.com>
2023-01-23 13:31:34 +0000

    fix(checkov): automatic updates and vm agents for bastion and shir

--- infrastructure/modules/synapse-shir/shir-vm.tf
+++ infrastructure/modules/synapse-shir/shir-vm.tf
@@ -15,6 +15,7 @@ resource "azurerm_network_interface" "shir" {
 resource "azurerm_windows_virtual_machine" "synapse" {
   #checkov:skip=CKV_AZURE_50: VM extensions are required for provisioning and updates
   #checkov:skip=CKV_AZURE_151: Host encryption is not supported
+  #checkov:skip=CKV_AZURE_177: Automatic updates are conditionally supported based on VM type
   name                  = "pins-vm-shir-${local.resource_suffix}"
   resource_group_name   = var.resource_group_name
   location              = var.location
@@ -22,9 +23,10 @@ resource "azurerm_windows_virtual_machine" "synapse" {
   admin_username        = var.runtime_vm_username
   admin_password        = random_password.shir_vm_administrator_password.result
   computer_name         = "shir-${random_string.unique_id.id}"
+  hotpatching_enabled   = contains(split("_", var.runtime_vm_size), "v2") ? true : false
   network_interface_ids = [azurerm_network_interface.shir.id]
   patch_mode            = contains(split("_", var.runtime_vm_size), "v2") ? "AutomaticByPlatform" : "AutomaticByOS"
-  hotpatching_enabled   = contains(split("_", var.runtime_vm_size), "v2") ? true : false
+  provision_vm_agent    = true
 
   identity {
     type = "SystemAssigned"
