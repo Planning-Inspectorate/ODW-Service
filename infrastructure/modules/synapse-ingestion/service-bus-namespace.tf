fd871f632b123c174456bfc7815b95e3f835c60b
Lester March <lestermarch@outlook.com>
2023-01-24 09:32:53 +0000

    fix(service-bus): add pins prefix to resource names

--- infrastructure/modules/synapse-ingestion/service-bus-namespace.tf
+++ infrastructure/modules/synapse-ingestion/service-bus-namespace.tf
@@ -2,7 +2,7 @@ resource "azurerm_servicebus_namespace" "synapse" {
   #checkov:skip=CKV_AZURE_199: Microsoft managed keys are acceptable
   #checkov:skip=CKV_AZURE_201: Microsoft managed keys are acceptable
   #checkov:skip=CKV_AZURE_204: Public access should be enabled
-  name                = "sb-${local.resource_suffix}-${random_string.unique_id.id}"
+  name                = "pins-sb-${local.resource_suffix}-${random_string.unique_id.id}"
   location            = var.location
   resource_group_name = var.resource_group_name
   sku                 = var.service_bus_failover_enabled ? "Premium" : "Standard"
--- infrastructure/modules/synapse-ingestion/service-bus-topics.tf
+++ infrastructure/modules/synapse-ingestion/service-bus-topics.tf
@@ -1,7 +1,7 @@
 resource "azurerm_servicebus_topic" "employee" {
   count = var.failover_namespace ? 0 : 1
 
-  name         = "Employee"
+  name         = "employee"
   namespace_id = azurerm_servicebus_namespace.synapse.id
 
   auto_delete_on_idle                     = "P10675199DT2H48M5.4775807S"
