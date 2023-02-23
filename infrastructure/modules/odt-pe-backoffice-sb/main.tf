fe24ee5fb60cb6d63cd1ed8f2993805b2aea6e11
adelany <alex.delany@optima-cc.com>
2023-02-23 17:33:40 +0000

    Updated var.odt_pe_backoffice_sb_failover_enabled

--- infrastructure/modules/odt-pe-backoffice-sb/main.tf
+++ infrastructure/modules/odt-pe-backoffice-sb/main.tf
@@ -6,12 +6,12 @@ resource "azurerm_private_endpoint" "odt_backoffice_servicebus_private_endpoint"
   private_service_connection {
     name                           = "pins-psc-backoffice-sb-${local.resource_suffix}"
     is_manual_connection           = false
-    private_connection_resource_id = data.azurerm_resources.odt_pe_backoffice_sb.resources[0].id
+    private_connection_resource_id = data.azurerm_resources.odt_pe_backoffice_sb.resources.id
     subresource_names              = ["namespace"]
   }
   private_dns_zone_group {
     name                 = "pins-pdns-backoffice-sb-${local.resource_suffix}"
-    private_dns_zone_ids = [var.odt_back_office_private_endpoint_dns_zone_id[0].id]
+    private_dns_zone_ids = [var.odt_back_office_private_endpoint_dns_zone_id]
   }
   tags = {
     environment = var.environment
