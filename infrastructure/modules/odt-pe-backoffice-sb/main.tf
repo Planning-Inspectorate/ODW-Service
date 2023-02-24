601b221e4b1f761760aca3281dda37528ec24ef0
adelany <alex.delany@optima-cc.com>
2023-02-24 10:08:34 +0000

    Updated provider and fixed syntax issues

--- infrastructure/modules/odt-pe-backoffice-sb/main.tf
+++ infrastructure/modules/odt-pe-backoffice-sb/main.tf
@@ -6,13 +6,13 @@ resource "azurerm_private_endpoint" "odt_backoffice_servicebus_private_endpoint"
   private_service_connection {
     name                           = "pins-psc-backoffice-sb-${local.resource_suffix}"
     is_manual_connection           = false
-    private_connection_resource_id = data.azurerm_resources.odt_pe_backoffice_sb.resources.id
+    private_connection_resource_id = data.azurerm_resources.odt_pe_backoffice_sb.resources[0].id
     subresource_names              = ["namespace"]
   }
   private_dns_zone_group {
     name                 = "pins-pdns-backoffice-sb-${local.resource_suffix}"
     private_dns_zone_ids = [var.odt_back_office_private_endpoint_dns_zone_id]
   }
-  
+
   tags = local.tags
 }
