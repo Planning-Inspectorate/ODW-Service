78ecb2f5d91f13e94824d4510c58f3c7d6d56dff
adelany <alex.delany@optima-cc.com>
2023-02-24 10:08:34 +0000

    Updated provider and fixed syntax issues

--- infrastructure/modules/odt-pe-backoffice-sb/data.tf
+++ infrastructure/modules/odt-pe-backoffice-sb/data.tf
@@ -1,5 +1,4 @@
 data "azurerm_resources" "odt_pe_backoffice_sb" {
-  provider            = azurerm.odt_backoffice
   resource_group_name = local.resource_group
   type                = "Microsoft.ServiceBus/namespaces"
 }
--- infrastructure/workload-odt-pe-backoffice-sb.tf
+++ infrastructure/workload-odt-pe-backoffice-sb.tf
@@ -39,10 +39,6 @@ resource "azurerm_private_dns_zone_virtual_network_link" "back_office_private_dn
 module "odt-pe-backoffice-sb" {
   source = "./modules/odt-pe-backoffice-sb"
 
-  providers = {
-    azurerm = azurerm.odt_backoffice
-  }
-
   environment                                              = var.environment
   resource_group_name                                      = azurerm_resource_group.odt_pe_backoffice_sb.name
   location                                                 = azurerm_resource_group.odt_pe_backoffice_sb.location
@@ -55,7 +51,6 @@ module "odt-pe-backoffice-sb" {
   synapse_private_endpoint_subnet_name                     = local.synapse_subnet_name
   synapse_private_endpoint_vnet_subnets                    = module.synapse_network.vnet_subnets
 
-
   tags = local.tags
 }
 
@@ -63,9 +58,6 @@ module "odt-pe-backoffice-sb-failover" {
   count = var.failover_deployment ? 1 : 0
 
   source = "./modules/odt-pe-backoffice-sb"
-  providers = {
-    azurerm = azurerm.odt_backoffice
-  }
 
   environment                                              = var.environment
   resource_group_name                                      = azurerm_resource_group.odt_pe_backoffice_sb_failover.name
