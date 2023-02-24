d362c9471ac4345c282cddb3e944423ae41ee892
adelany <alex.delany@optima-cc.com>
2023-02-24 09:27:22 +0000

    Fix for provider for odt_pe_backoffice_sb

--- infrastructure/modules/odt-pe-backoffice-sb/data.tf
+++ infrastructure/modules/odt-pe-backoffice-sb/data.tf
@@ -1,5 +1,5 @@
 data "azurerm_resources" "odt_pe_backoffice_sb" {
-  azure_provider      = azurerm.odt_backoffice
+  provider            = azurerm.odt_backoffice
   resource_group_name = local.resource_group
   type                = "Microsoft.ServiceBus/namespaces"
 }
