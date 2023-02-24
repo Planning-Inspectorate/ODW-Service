50ca4a8e65998352728d5d4067700bf1d1354a03
adelany <alex.delany@optima-cc.com>
2023-02-24 09:34:01 +0000

    Fix provider issue

---
+++ infrastructure/modules/odt-pe-backoffice-sb/provider.tf
@@ -0,0 +1,33 @@
+terraform {
+  backend "azurerm" {
+    # Per-environment key specified in ./environments/*.tfbackend
+    container_name       = "terraformstate-odw"
+    resource_group_name  = "pins-rg-shared-terraform-uks"
+    subscription_id      = "edb1ff78-90da-4901-a497-7e79f966f8e2"
+    storage_account_name = "pinsstsharedtfstateuks"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 1.1.6, < 2.0.0"
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id            = var.odt_back_office_subscription_id
+  skip_provider_registration = true
+  alias                      = "odt_backoffice"
+}
