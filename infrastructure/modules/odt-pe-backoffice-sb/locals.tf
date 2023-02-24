9c9f591d4e2658bea19477d89a7761e11171ad66
adelany <alex.delany@optima-cc.com>
2023-02-24 08:40:56 +0000

    Updated variables for odt-pe-backoffice-sb

--- infrastructure/modules/odt-pe-backoffice-sb/locals.tf
+++ infrastructure/modules/odt-pe-backoffice-sb/locals.tf
@@ -1,7 +1,7 @@
 locals {
   module_name     = "odt-pe-backoffice-sb"
   resource_suffix = "${var.service_name}-${var.environment}-${module.azure_region.location_short}"
-  resource_group  = var.odt_back_office_service_bus_failover_enabled == true ? var.odt_pe_backoffice_service_bus_failover_resource_group : var.odt_pe_backoffice_service_bus_resource_group
+  resource_group  = var.odt_back_office_service_bus_failover_enabled == true ? var.odt_pe_backoffice_service_bus_resource_group_name_failover : var.odt_pe_backoffice_service_bus_resource_group_name
 
   tags = merge(
     var.tags,
