692a04c80318db30d80f7d7ff7051ddc21ec340b
adelany <alex.delany@optima-cc.com>
2023-03-01 21:22:18 +0000

    Updated syntax

--- infrastructure/modules/odt-pe-backoffice-sb/data.tf
+++ infrastructure/modules/odt-pe-backoffice-sb/data.tf
@@ -1,6 +1,6 @@
 data "azurerm_resources" "odt_pe_backoffice_sb" {
   provider            = azurerm.odt_backoffice
-  resource_group_name = var.odt_back_office_service_bus_name
-  name                = var.odt_back_office_service_bus_resource_group_name
+  resource_group_name = var.odt_back_office_service_bus_resource_group_name
+  name                = var.odt_back_office_service_bus_name
   type                = "Microsoft.ServiceBus/namespaces"
 }
