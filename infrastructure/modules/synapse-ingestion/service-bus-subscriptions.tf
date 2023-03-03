94dc4837dc7c871d0d5f7f2f44e6235aba7aff2d
adelany <alex.delany@optima-cc.com>
2023-03-03 17:38:46 +0000

    Updated count syntax

--- infrastructure/modules/synapse-ingestion/service-bus-subscriptions.tf
+++ infrastructure/modules/synapse-ingestion/service-bus-subscriptions.tf
@@ -1,7 +1,7 @@
 resource "azurerm_servicebus_subscription" "topic_subscriptions" {
   count = var.failover_namespace ? 0 : var.service_bus_topics_and_subscriptions == null ? 0 : length(var.service_bus_topics_and_subscriptions)
 
-  name               = element(values(var.service_bus_topics_and_subscriptions), count.index)
+  name               = keys(values(var.service_bus_topics_and_subscriptions)[count.index])[0]
   topic_id           = azurerm_servicebus_topic.topics[count.index].id
   max_delivery_count = 1
 }
