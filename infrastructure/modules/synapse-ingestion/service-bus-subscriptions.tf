d1666732b55356587c25295eaf823a3c46eb2596
adelany <alex.delany@optima-cc.com>
2023-03-06 14:04:23 +0000

    Updated with output test

--- infrastructure/modules/synapse-ingestion/service-bus-subscriptions.tf
+++ infrastructure/modules/synapse-ingestion/service-bus-subscriptions.tf
@@ -1,7 +1,7 @@
-resource "azurerm_servicebus_subscription" "topic_subscriptions" {
-  for_each = var.failover_namespace ? 0 : var.service_bus_topics_and_subscriptions == null ? 0 : var.service_bus_topics_and_subscriptions
+# resource "azurerm_servicebus_subscription" "topic_subscriptions" {
+#   for_each = var.failover_namespacee ? 0 : var.service_bus_topics_and_subscriptions == null ? 0 : var.service_bus_topics_and_subscriptions
 
-  name               = each.value
-  topic_id           = azurerm_servicebus_topic.topics[each.key].id
-  max_delivery_count = 1
-}
+#   name               = each.value
+#   topic_id           = azurerm_servicebus_topic.topics[each.key].id
+#   max_delivery_count = 1
+# }
