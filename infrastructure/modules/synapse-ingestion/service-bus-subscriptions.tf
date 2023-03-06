df1ba362c27e4cb439de7b5473ad65b4ba28fa60
adelany <alex.delany@optima-cc.com>
2023-03-06 17:06:41 +0000

    Updated sb subscription

--- infrastructure/modules/synapse-ingestion/service-bus-subscriptions.tf
+++ infrastructure/modules/synapse-ingestion/service-bus-subscriptions.tf
@@ -1,6 +1,6 @@
 resource "azurerm_servicebus_subscription" "topic_subscriptions" {
   for_each = var.failover_namespace ? {} : {
-    for topic in local.service_bus_topics_and_subscriptions : "${topic.topic_name}.${topic.subscription_name}" => topic.subscription_name
+    for topic in local.service_bus_topics_and_subscriptions : "${topic.topic_name}.${topic.subscription_name}" => topic
   }
 
   name               = each.value.subscription_name
