98cbb8d8da3f8c9b27290ccf9147359460dc80a9
adelany <alex.delany@optima-cc.com>
2023-03-06 21:18:00 +0000

    Updated syntax

--- infrastructure/modules/synapse-ingestion/service-bus-subscriptions.tf
+++ infrastructure/modules/synapse-ingestion/service-bus-subscriptions.tf
@@ -1,9 +1,9 @@
 resource "azurerm_servicebus_subscription" "topic_subscriptions" {
   for_each = var.failover_namespace ? {} : {
-    for subscriptions in var.service_bus_topics_and_subscriptions : "${subscriptions.topic_name}.${subscriptions.subscription_name}" => subscriptions
+    for subscriptions in local.service_bus_topics_and_subscriptions : "${subscriptions.topic_name}.${subscriptions.subscription_name}" => subscriptions
   }
 
   name               = each.value.subscription_name
-  topic_id           = azurerm_servicebus_topic.topics[each.key].id
+  topic_id           = azurerm_servicebus_topic.topics[each.value.topic_name].id
   max_delivery_count = 1
 }
