3907bf5bc6ab3e6ac56cee418ff2cbcd89471f0a
adelany <alex.delany@optima-cc.com>
2023-03-06 14:34:41 +0000

    for_each variable error fixed

--- infrastructure/modules/synapse-ingestion/service-bus-subscriptions.tf
+++ infrastructure/modules/synapse-ingestion/service-bus-subscriptions.tf
@@ -1,5 +1,5 @@
 resource "azurerm_servicebus_subscription" "topic_subscriptions" {
-  for_each = var.service_bus == false ? {} : {
+  for_each = var.failover_namespace == false ? {} : {
     for topic in local.service_bus_topics_and_subscriptions : "${topic.topic_name}.${topic.subscription_name}" => topic.subscription_name
   }
 
