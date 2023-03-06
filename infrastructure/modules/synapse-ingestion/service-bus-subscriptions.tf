36adbfd6ea4ce2249aa71d6121a1227de3b3b9ab
adelany <alex.delany@optima-cc.com>
2023-03-06 13:43:04 +0000

    Updated syntax to use for_each

--- infrastructure/modules/synapse-ingestion/service-bus-subscriptions.tf
+++ infrastructure/modules/synapse-ingestion/service-bus-subscriptions.tf
@@ -1,7 +1,7 @@
 resource "azurerm_servicebus_subscription" "topic_subscriptions" {
-  count = var.failover_namespace ? 0 : var.service_bus_topics_and_subscriptions == null ? 0 : length(var.service_bus_topics_and_subscriptions)
+  for_each = var.failover_namespace ? 0 : var.service_bus_topics_and_subscriptions == null ? 0 : var.service_bus_topics_and_subscriptions
 
-  name               = keys(values(var.service_bus_topics_and_subscriptions)[count.index])[0]
-  topic_id           = azurerm_servicebus_topic.topics[count.index].id
+  name               = each.key
+  topic_id           = azurerm_servicebus_topic.topics[each.key].id
   max_delivery_count = 1
 }
