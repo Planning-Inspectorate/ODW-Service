e3ec16225626f594a570a0d8b04bd76fc92153d1
adelany <alex.delany@optima-cc.com>
2023-03-06 19:39:13 +0000

    Updated syntax issue

--- infrastructure/modules/synapse-ingestion/service-bus-subscriptions.tf
+++ infrastructure/modules/synapse-ingestion/service-bus-subscriptions.tf
@@ -2,7 +2,7 @@ resource "azurerm_servicebus_subscription" "topic_subscriptions" {
   for_each = var.failover_namespace ? {} : {
     for subscription in local.service_bus_topics_and_subscriptions : "${subscription.topic_name}.${subscription.subscription_name}" => subscription
   }
-  
+
   name               = each.value.subscription_name
   topic_id           = azurerm_servicebus_topic.topics[each.value.topic_name].id
   max_delivery_count = 1
