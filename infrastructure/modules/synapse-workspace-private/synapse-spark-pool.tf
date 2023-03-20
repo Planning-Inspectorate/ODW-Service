83f54db6b52e7334d5c99dae9aabec75a18b8b1c
Kathryn Thompson <kathryn.thompson@kinandcarta.com>
2023-03-20 14:42:41 +0000

    Update synapse-spark-pool.tf

--- infrastructure/modules/synapse-workspace-private/synapse-spark-pool.tf
+++ infrastructure/modules/synapse-workspace-private/synapse-spark-pool.tf
@@ -31,6 +31,7 @@ resource "azurerm_synapse_spark_pool" "synapse" {
       spark.executorEnv.dataLakeAccountName ${var.data_lake_account_name}
       spark.executorEnv.keyVaultName ${var.key_vault_name}
       spark.microsoft.delta.merge.lowShuffle.enabled false
+      spark.databricks.delta.optimize.repartition.enabled true
       EOT
     filename = "configuration.txt"
   }
