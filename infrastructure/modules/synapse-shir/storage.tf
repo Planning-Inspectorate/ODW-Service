39a2c1e14d4520af1cc7cca7b82b07ef0472bfb5
nasir-rahman9 <nasirrahman@live.co.uk>
2023-04-03 13:27:45 +0100

    added checkov skips

--- infrastructure/modules/synapse-shir/storage.tf
+++ infrastructure/modules/synapse-shir/storage.tf
@@ -1,5 +1,4 @@
 resource "azurerm_storage_account" "shir" {
-  #checkov:skip=CKV_AZURE_33: Firewall not used on this storage account to implement private endpoint
   #checkov:skip=CKV_AZURE_35: Firewall not required for this stroage account
   #checkov:skip=CKV_AZURE_59: Firewall not required for this stroage account
   #checkov:skip=CKV_AZURE_190: Firewall not required for this stroage account
@@ -7,6 +6,7 @@ resource "azurerm_storage_account" "shir" {
   #checkov:skip=CKV2_AZURE_1: Microsoft managed keys are acceptable
   #checkov:skip=CKV2_AZURE_8: Firewall not required for this stroage account
   #checkov:skip=CKV2_AZURE_18: Microsoft managed keys are acceptable
+  #checkov:skip=CKV2_AZURE_33: Firewall not used on this storage account to implement private endpoint
   name                            = replace("pins-st-${local.resource_suffix}-${random_string.unique_id.id}", "-", "")
   resource_group_name             = var.resource_group_name
   location                        = var.location
