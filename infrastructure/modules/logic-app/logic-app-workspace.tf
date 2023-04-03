a89d0683f37b4050d12c28540755f1036bae27c6
nasir-rahman9 <nasirrahman@live.co.uk>
2023-04-03 11:02:03 +0100

    updated branch with merged main branch

---
+++ infrastructure/modules/logic-app/logic-app-workspace.tf
@@ -0,0 +1,11 @@
+# resource "azurerm_logic_app_workflow" "workflow" {
+#   name                = "zendesk-created"
+#   location            = var.location
+#   resource_group_name = var.resource_group_name
+# }
+
+# resource "azurerm_logic_app_workflow" "workflow" {
+#   name                = ""
+#   location            = var.location
+#   resource_group_name = var.resource_group_name
+# }
