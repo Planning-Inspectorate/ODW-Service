9cd609c324c7a1a89337f407b7455abb9582e05b
Maria Valero Gonzalez <97243445+MariaValeroGonzalezKC@users.noreply.github.com>
2023-04-06 17:17:18 +0200

    Mvg/fix odw merge ms solution (#425)
    * taking out non-working solutions
    
    * adding ms solution for the merge error
    
    ---------
    
    Co-authored-by: abdullah-p <44368581+abdullah-p@users.noreply.github.com>

--- infrastructure/modules/synapse-workspace-private/synapse-spark-pool.tf
+++ infrastructure/modules/synapse-workspace-private/synapse-spark-pool.tf
@@ -30,9 +30,8 @@ resource "azurerm_synapse_spark_pool" "synapse" {
     content  = <<-EOT
       spark.executorEnv.dataLakeAccountName ${var.data_lake_account_name}
       spark.executorEnv.keyVaultName ${var.key_vault_name}
-      spark.microsoft.delta.merge.lowShuffle.enabled false
-      spark.databricks.delta.optimize.repartition.enabled true
       spark.sql.parquet.int96RebaseModeInWrite CORRECTED
+      spark.sql.constraintPropagation.enabled false
       EOT
     filename = "configuration.txt"
   }
