1a3c48c6c155cfe3903695d1d39b6949b3fb0e5d
nasir-rahman9 <nasirrahman@live.co.uk>
2023-04-12 11:52:59 +0100

    updated functions extension version

--- infrastructure/modules/logic-app/logic-app-standard.tf
+++ infrastructure/modules/logic-app/logic-app-standard.tf
@@ -10,6 +10,7 @@ resource "azurerm_logic_app_standard" "logic_app" {
 
   app_settings = {
     "FUNCTIONS_WORKER_RUNTIME"     = "node"
+    "FUNCTIONS_EXTENSION_VERSION"  = "~4"
     "WEBSITE_NODE_DEFAULT_VERSION" = "~16"
   }
 
