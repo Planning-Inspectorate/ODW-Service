24826af8f01ee49cfdf3c75bd3b495bc99a3a476
nasir-rahman9 <122629783+nasir-rahman9@users.noreply.github.com>
2023-04-24 16:08:33 +0100

    feat/odw-483-logic-app-workflows (#472)
    * updated workflow code
    
    * updated names of workflow
    
    * updated names of workflow
    
    * add api connection in terraform
    
    * create logic app workflows and api connections
    
    * pushed up information on workload

---
+++ infrastructure/modules/logic-app/data.tf
@@ -0,0 +1,9 @@
+data "azurerm_managed_api" "servicebus2" {
+  name     = "servicebus-2"
+  location = var.location
+}
+
+data "azurerm_managed_api" "zendesk" {
+  name     = "zendesk"
+  location = var.location
+}
