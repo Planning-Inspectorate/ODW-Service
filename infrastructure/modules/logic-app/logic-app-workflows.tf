1c5f3032f874b373d601372234bef925123f0262
abdullah-p <44368581+abdullah-p@users.noreply.github.com>
2023-04-26 15:25:09 +0100

    Revert "feat/odw-483-logic-app-workflows (#472)"
    This reverts commit 24826af8f01ee49cfdf3c75bd3b495bc99a3a476.

--- infrastructure/modules/logic-app/logic-app-workflows.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_logic_app_workflow" "workflows" {
-  for_each = var.workflow_names
-
-  name                = each.value
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  tags                = local.tags
-}
