26b027bcc7ddc038f5a57a9e1abba68a603fd3fa
nasir-rahman9 <nasirrahman@live.co.uk>
2023-05-04 16:48:08 +0100

    hashed out api connections

--- infrastructure/modules/logic-app/api-connection.tf
+++ infrastructure/modules/logic-app/api-connection.tf
@@ -1,37 +1,37 @@
-resource "azurerm_api_connection" "servicebus" {
-  count               = var.api_connection_servicebus_enabled ? 1 : 0
-  name                = "servicebus"
-  resource_group_name = var.resource_group_name
-  managed_api_id      = data.azurerm_managed_api.servicebus.id
-  display_name        = "ESB"
+# resource "azurerm_api_connection" "servicebus" {
+#   count               = var.api_connection_servicebus_enabled ? 1 : 0
+#   name                = "servicebus"
+#   resource_group_name = var.resource_group_name
+#   managed_api_id      = data.azurerm_managed_api.servicebus.id
+#   display_name        = "ESB"
 
-  parameter_values = {
-    connectionString = var.connection_string_servicebus
-  }
+#   parameter_values = {
+#     connectionString = var.connection_string_servicebus
+#   }
 
-  tags = local.tags
+#   tags = local.tags
 
-  lifecycle {
-    # NOTE: since the connectionString is a secure value it's not returned from the API
-    ignore_changes = [parameter_values]
-  }
-}
+#   lifecycle {
+#     # NOTE: since the connectionString is a secure value it's not returned from the API
+#     ignore_changes = [parameter_values]
+#   }
+# }
 
-resource "azurerm_api_connection" "zendesk" {
-  count               = var.api_connection_zendesk_enabled ? 1 : 0
-  name                = "zendesk"
-  resource_group_name = var.resource_group_name
-  managed_api_id      = data.azurerm_managed_api.zendesk.id
-  display_name        = "pinssupport"
+# resource "azurerm_api_connection" "zendesk" {
+#   count               = var.api_connection_zendesk_enabled ? 1 : 0
+#   name                = "zendesk"
+#   resource_group_name = var.resource_group_name
+#   managed_api_id      = data.azurerm_managed_api.zendesk.id
+#   display_name        = "pinssupport"
 
-  parameter_values = {
-    connectionString = var.connection_string_zendesk
-  }
+#   parameter_values = {
+#     connectionString = var.connection_string_zendesk
+#   }
 
-  tags = local.tags
+#   tags = local.tags
 
-  lifecycle {
-    # NOTE: since the connectionString is a secure value it's not returned from the API
-    ignore_changes = [parameter_values]
-  }
-}
+#   lifecycle {
+#     # NOTE: since the connectionString is a secure value it's not returned from the API
+#     ignore_changes = [parameter_values]
+#   }
+# }
