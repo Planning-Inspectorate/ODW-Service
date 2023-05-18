d64aaa4dc8101a60fd59e1af655c623b9949527c
adelany <alex.delany@optima-cc.com>
2023-05-18 20:51:34 +0100

    Updated syntax

--- infrastructure/modules/logic-app/app-service-plan.tf
+++ infrastructure/modules/logic-app/app-service-plan.tf
@@ -1,6 +1,6 @@
 resource "azurerm_service_plan" "logic_app" {
   count = var.logic_app_enabled ? 1 : 0
-  
+
   name                = "pins-app-service-${local.resource_suffix}"
   location            = var.location
   resource_group_name = var.resource_group_name
