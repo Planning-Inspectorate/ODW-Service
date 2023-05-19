488f1765b56228c3ba2ebfcd9344c7e8ed5b92d7
adelany <alex.delany@optima-cc.com>
2023-05-19 14:03:25 +0100

    Updated syntax

--- infrastructure/modules/logic-app/app-service-plan.tf
+++ infrastructure/modules/logic-app/app-service-plan.tf
@@ -1,24 +1,12 @@
-# resource "azurerm_service_plan" "logic_app" {
-#   count = var.logic_app_enabled ? 1 : 0
-
-#   name                = "pins-app-service-${local.resource_suffix}"
-#   location            = var.location
-#   resource_group_name = var.resource_group_name
-#   os_type             = "Windows"
-#   sku_name            = "WS1"
-# }
-
 resource "azurerm_app_service_plan" "logic_app" {
-  count = var.logic_app_enabled ? 1 : 0
-
-  name                = "pins-app-service-${local.resource_suffix}"
+  count               = var.logic_app_enabled ? 1 : 0
+  name                = "pins-logic-app-${local.resource_suffix}"
   location            = var.location
   resource_group_name = var.resource_group_name
-  kind                = "elastic"
-
+  tags                = local.tags
 
   sku {
-    tier = "WorkflowStandard"
+    tier = "Standard"
     size = "WS1"
   }
 }
