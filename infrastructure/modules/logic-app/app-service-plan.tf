0d763ce3bef89ca61ba06f06ee5ca4899d31fc66
adelany <alex.delany@optima-cc.com>
2023-05-19 14:33:43 +0100

    Updated with service plan

--- infrastructure/modules/logic-app/app-service-plan.tf
+++ infrastructure/modules/logic-app/app-service-plan.tf
@@ -1,12 +1,9 @@
-resource "azurerm_app_service_plan" "logic_app" {
+resource "azurerm_service_plan" "logic_app" {
   count               = var.logic_app_enabled ? 1 : 0
-  name                = "pins-logic-app-${local.resource_suffix}"
+  name                = "pins-service-plan-${local.resource_suffix}"
   location            = var.location
   resource_group_name = var.resource_group_name
+  os_type             = "Windows"
+  sku_name            = "WS1"
   tags                = local.tags
-
-  sku {
-    tier = "Standard"
-    size = "WS1"
-  }
 }
