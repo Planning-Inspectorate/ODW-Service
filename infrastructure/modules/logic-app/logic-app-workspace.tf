32f1c93089385be87634bb94163ec30810cb4cd8
adelany <alex.delany@optima-cc.com>
2023-05-19 17:21:49 +0100

    Updated formatting

--- infrastructure/modules/logic-app/logic-app-workspace.tf
+++ infrastructure/modules/logic-app/logic-app-workspace.tf
@@ -7,62 +7,62 @@ resource "azurerm_logic_app_workflow" "zendesk_created" {
 }
 
 resource "azurerm_logic_app_action_custom" "zendesk_created" {
-  count              = var.logic_app_enabled ? 1 : 0
+  count = var.logic_app_enabled ? 1 : 0
 
-  name               = "send-message"
-  logic_app_id       = azurerm_logic_app_workflow.zendesk_created[count.index].id
+  name         = "send-message"
+  logic_app_id = azurerm_logic_app_workflow.zendesk_created[count.index].id
 
   body = jsonencode(
-        {
-            "Send_message": {
-                "inputs": {
-                    "body": {
-                        "ContentData": "@{base64(triggerOutputs())}",
-                        "Label": "Created",
-                        "MessageId": "@{guid()}",
-                        "SessionId": "@{guid()}"
-                    },
-                    "host": {
-                        "connection": {
-                            "referenceName": "servicebus-2"
-                        }
-                    },
-                    "method": "post",
-                    "path": "/@{encodeURIComponent(encodeURIComponent('zendesk'))}/messages"
-                },
-                "runAfter": {},
-                "type": "ApiConnection"
+    {
+      "Send_message" : {
+        "inputs" : {
+          "body" : {
+            "ContentData" : "@{base64(triggerOutputs())}",
+            "Label" : "Created",
+            "MessageId" : "@{guid()}",
+            "SessionId" : "@{guid()}"
+          },
+          "host" : {
+            "connection" : {
+              "referenceName" : "servicebus-2"
             }
-        }
-    )
+          },
+          "method" : "post",
+          "path" : "/@{encodeURIComponent(encodeURIComponent('zendesk'))}/messages"
+        },
+        "runAfter" : {},
+        "type" : "ApiConnection"
+      }
+    }
+  )
 }
 
 resource "azurerm_logic_app_trigger_custom" "zendesk_created" {
-    count = var.logic_app_enabled ? 1 : 0
+  count = var.logic_app_enabled ? 1 : 0
 
-    name = "When_an_item_is_created"
-    logic_app_id = azurerm_logic_app_workflow.zendesk_created[count.index].id
+  name         = "When-an-item-is-created"
+  logic_app_id = azurerm_logic_app_workflow.zendesk_created[count.index].id
 
-    body = jsonencode(
-        {
-            "When_an_item_is_created": {
-                "inputs": {
-                    "host": {
-                        "connection": {
-                            "referenceName": "zendesk"
-                        }
-                    },
-                    "method": "get",
-                    "path": "/datasets/default/tables/@{encodeURIComponent(encodeURIComponent('tickets'))}/onnewitems"
-                },
-                "recurrence": {
-                    "frequency": "Minute",
-                    "interval": 1
-                },
-                "type": "ApiConnection"
+  body = jsonencode(
+    {
+      "When_an_item_is_created" : {
+        "inputs" : {
+          "host" : {
+            "connection" : {
+              "referenceName" : "zendesk"
             }
-        }
-    )
+          },
+          "method" : "get",
+          "path" : "/datasets/default/tables/@{encodeURIComponent(encodeURIComponent('tickets'))}/onnewitems"
+        },
+        "recurrence" : {
+          "frequency" : "Minute",
+          "interval" : 1
+        },
+        "type" : "ApiConnection"
+      }
+    }
+  )
 }
 
 # resource "azurerm_logic_app_workflow" "zendesk_updated" {
