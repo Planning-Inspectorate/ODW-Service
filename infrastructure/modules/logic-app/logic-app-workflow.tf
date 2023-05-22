f3f1f2407ff4b57ad47edcc7435733c1eab0d4fe
adelany <alex.delany@optima-cc.com>
2023-05-22 21:38:47 +0100

    Updated with workflow

--- infrastructure/modules/logic-app/logic-app-workflow.tf
+++ infrastructure/modules/logic-app/logic-app-workflow.tf
@@ -1,10 +1,62 @@
-# resource "azurerm_logic_app_workflow" "zendesk_created" {
-#   count               = var.logic_app_enabled ? 1 : 0
-#   name                = "zendesk-created"
-#   location            = var.location
-#   resource_group_name = var.resource_group_name
-#   tags                = local.tags
-# }
+resource "azurerm_logic_app_workflow" "zendesk_created" {
+  count               = var.logic_app_enabled ? 1 : 0
+  name                = "zendesk-created"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tags                = local.tags
+}
+
+resource "azurerm_logic_app_action_custom" "action" {
+  count = var.logic_app_enabled ? 1 : 0
+
+  name         = "zendesk-created"
+  logic_app_id = azurerm_logic_app_workflow.zendesk_created[count.index].id
+
+  body = <<BODY
+  {
+    "actions": {
+      "Send_message": {
+        "type": "ApiConnection",
+        "inputs": {
+          "host": {
+            "connection": {
+              "referenceName": "servicebus-1"
+            }
+          },
+          "method": "post",
+          "body": {
+            "SessionId": "@{guid()}",
+            "MessageId": "@{guid()}",
+            "Label": "Created",
+            "ContentData": "@{base64(triggerOutputs())}"
+          },
+          "path": "/@{encodeURIComponent(encodeURIComponent('zendesk'))}/messages"
+        },
+        "runAfter": {}
+      }
+    }
+  }
+BODY
+
+}
+
+resource "azurerm_logic_app_trigger_custom" "trigger" {
+  count = var.logic_app_enabled ? 1 : 0
+
+  name         = "zendesk-created"
+  logic_app_id = azurerm_logic_app_workflow.zendesk_created[count.index].id
+
+  body = <<BODY
+  {
+  "recurrence": {
+    "frequency": "Day",
+    "interval": 1
+  },
+  "type": "Recurrence"
+ }
+BODY
+
+}
 
 # resource "azurerm_logic_app_workflow" "zendesk_updated" {
 #   count               = var.logic_app_enabled ? 1 : 0
