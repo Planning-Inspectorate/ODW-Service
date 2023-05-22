13e80ca025d2e8f8ea0a5d9af879a5510a9487a6
adelany <alex.delany@optima-cc.com>
2023-05-22 16:48:52 +0100

    Update api connection

--- infrastructure/modules/logic-app/api-connections.tf
+++ infrastructure/modules/logic-app/api-connections.tf
@@ -1,11 +1,11 @@
 resource "azurerm_resource_group_template_deployment" "service_bus_3" {
-  name                = "servicebus-3"
+  name                = "servicebus-1"
   resource_group_name = var.resource_group_name
 
-  template_content = file("${path.module}/api-connections/servicebus-3.json")
+  template_content = file("${path.module}/api-connections/servicebus.json")
   parameters_content = jsonencode({
     "service_bus_connection_name" = {
-      value = "servicebus-3"
+      value = "servicebus-1"
     }
     "service_bus_api_id" = {
       value = module.synapse_ingestion.service_bus_namespace_id
