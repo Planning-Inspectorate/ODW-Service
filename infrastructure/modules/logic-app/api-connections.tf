87b6933f74e2709fc092b8eb74529e204bd634c1
nasir-rahman9 <nasirrahman@live.co.uk>
2023-05-22 11:44:04 +0100

    updated the api connection files

--- infrastructure/modules/logic-app/api-connections.tf
+++ infrastructure/modules/logic-app/api-connections.tf
@@ -1,7 +1,15 @@
-resource "azurerm_resource_group_template_deployment" "service_bus_2" {
-  name                = "servicebus-2"
+resource "azurerm_resource_group_template_deployment" "service_bus_3" {
+  name                = "servicebus-3"
   resource_group_name = var.resource_group_name
 
-  template_content = file("${path.module}/api-connections/servicebus-2.json")
+  template_content = file("${path.module}/api-connections/servicebus-3.json")
+  deployment_mode  = "Incremental"
+}
+
+resource "azurerm_resource_group_template_deployment" "zendesk" {
+  name                = "zendesk"
+  resource_group_name = var.resource_group_name
+
+  template_content = file("${path.module}/api-connections/zendesk.json")
   deployment_mode  = "Incremental"
 }
