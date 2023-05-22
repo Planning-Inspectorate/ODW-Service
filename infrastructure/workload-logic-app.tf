44b6ca5b5679d4e7e5cf15c63183bcb1640356d8
adelany <alex.delany@optima-cc.com>
2023-05-22 17:35:05 +0100

    Updated

--- infrastructure/workload-logic-app.tf
+++ infrastructure/workload-logic-app.tf
@@ -25,7 +25,7 @@ module "logic_app" {
   resource_group_name = azurerm_resource_group.logic_app[0].name
   location            = module.azure_region.location_cli
   logic_app_enabled   = var.logic_app_enabled
-  service_bus_id      = module.synapse.service_bus_namespace_id
+  service_bus_id      = module.synapse_ingestion.service_bus_namespace_id
   service_name        = local.service_name
 
   tags = local.tags
@@ -39,7 +39,7 @@ module "logic_app_failover" {
   environment         = var.environment
   resource_group_name = azurerm_resource_group.logic_app_failover[0].name
   location            = module.azure_region.paired_location.location_cli
-  service_bus_id      = module.synapse.service_bus_namespace_id
+  service_bus_id      = module.synapse_ingestion.service_bus_namespace_id
   logic_app_enabled   = var.logic_app_enabled
   service_name        = local.service_name
 
