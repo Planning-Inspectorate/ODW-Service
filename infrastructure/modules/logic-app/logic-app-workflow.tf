6bd8eb137a93d48c5fdc3380f564ca2090c2353c
adelany <alex.delany@optima-cc.com>
2023-05-24 17:31:07 +0100

    Removed standard logic app

--- infrastructure/modules/logic-app/logic-app-workflow.tf
+++ infrastructure/modules/logic-app/logic-app-workflow.tf
@@ -1,43 +1,46 @@
 resource "azurerm_logic_app_workflow" "zendesk_created" {
-  count               = var.logic_app_enabled ? 1 : 0
+  count = var.logic_app_enabled ? 1 : 0
+
   name                = "zendesk-created"
   location            = var.location
   resource_group_name = var.resource_group_name
   tags                = local.tags
 }
 
-resource "azurerm_logic_app_action_custom" "action" {
-  count = var.logic_app_enabled ? 1 : 0
+# resource "azurerm_logic_app_action_custom" "action" {
+#   count = var.logic_app_enabled ? 1 : 0
 
-  name         = "zendesk-action"
-  logic_app_id = azurerm_logic_app_workflow.zendesk_created[count.index].id
+#   name         = "zendesk-action"
+#   logic_app_id = azurerm_logic_app_workflow.zendesk_created[count.index].id
 
-  body = jsonencode({
-    "actions" : {
-      "Send_message" : {
-        "type" : "ApiConnection",
-        "inputs" : {
-          "host" : {
-            "connection" : {
-              "referenceName" : "servicebus-1"
-            }
-          },
-          "method" : "post",
-          "body" : {
-            "SessionId" : "@{guid()}",
-            "MessageId" : "@{guid()}",
-            "Label" : "Created",
-            "ContentData" : "@{base64(triggerOutputs())}"
-          },
-          "path" : "/@{encodeURIComponent(encodeURIComponent('zendesk'))}/messages"
-        },
-        "runAfter" : {}
-      }
-    },
-    "contentVersion" : "1.0.0.0",
-    "outputs" : {},
-  })
-}
+#   body = jsonencode({
+#     "actions" : {
+#       "Send_message" : {
+#         "runAfter" : {},
+#         "type" : "ApiConnection",
+#         "inputs" : {
+#           "body" : {
+#             "ContentData" : "@{base64(triggerOutputs())}",
+#             "Label" : "Created",
+#             "MessageId" : "@{guid()}",
+#             "SessionId" : "@{guid()}"
+#           },
+#           "host" : {
+#             "connection" : {
+#               "name" : "@parameters('$connections')['servicebus']['connectionId']"
+#             }
+#           },
+#           "method" : "post",
+#           "path" : "/@{encodeURIComponent(encodeURIComponent('zendesk'))}/messages",
+#           "queries" : {
+#             "systemProperties" : "None"
+#           }
+#         }
+#       }
+#     },
+#     "outputs" : {}
+#   })
+# }
 
 # resource "azurerm_logic_app_workflow" "zendesk_updated" {
 #   count               = var.logic_app_enabled ? 1 : 0
