8efb56e1bbebccff51fa544f8f79f13e2edfc622
nasir-rahman9 <nasirrahman@live.co.uk>
2023-05-24 12:22:51 +0100

    added managed private endpoints for key vault

---
+++ infrastructure/modules/synapse-management/key-vault-managed-private-endpoint.tf
@@ -0,0 +1,8 @@
+resource "azurerm_synapse_managed_private_endpoint" "key_vault" {
+  name                 = "synapse-mpe-kv-${var.key_vault_name}"
+  synapse_workspace_id = azurerm_synapse_workspace.synapse.id
+  target_resource_id   = var.key_vault_id
+  subresource_name     = "vault"
+
+  tags = local.tags
+}
