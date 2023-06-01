b77a0a96f122cf05f5f1aa11729d0636b011a55a
nasir-rahman9 <nasirrahman@live.co.uk>
2023-06-01 14:43:50 +0100

    add managed private endpoints

--- infrastructure/modules/synapse-workspace-private/synapse-private-endpoints.tf
+++ infrastructure/modules/synapse-workspace-private/synapse-private-endpoints.tf
@@ -113,3 +113,10 @@ resource "azurerm_synapse_managed_private_endpoint" "data_lake_failover" {
     time_sleep.firewall_delay
   ]
 }
+
+resource "azurerm_synapse_managed_private_endpoint" "synapse_mpe_kv" {
+  name                 = "synapse-mpe-kv-${var.key_vault_name}"
+  synapse_workspace_id = azurerm_synapse_workspace.synapse.id
+  target_resource_id   = var.key_vault_id
+  subresource_name     = "vault"
+}
