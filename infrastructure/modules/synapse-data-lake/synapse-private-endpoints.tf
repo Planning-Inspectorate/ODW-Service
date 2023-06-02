01f0c88aa2905574623c8d3f681b347378dd5a4a
nasir-rahman9 <nasirrahman@live.co.uk>
2023-06-02 15:24:51 +0100

    updated name for failover mpe

--- infrastructure/modules/synapse-data-lake/synapse-private-endpoints.tf
+++ infrastructure/modules/synapse-data-lake/synapse-private-endpoints.tf
@@ -6,7 +6,7 @@ resource "azurerm_synapse_managed_private_endpoint" "synapse_mpe_kv" {
 }
 
 resource "azurerm_synapse_managed_private_endpoint" "synapse_mpe_kv_failover" {
-  name                 = "synapse-mpe-kv-${var.key_vault_name}"
+  name                 = "synapse-failover-mpe-kv-${var.key_vault_name}"
   synapse_workspace_id = var.synapse_workspace_id
   target_resource_id   = azurerm_key_vault.synapse.id
   subresource_name     = "vault"
