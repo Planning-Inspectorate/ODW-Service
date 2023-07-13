6b44e12aee1643429be71002efab400ee3d8bbfc
adelany <alex.delany@optima-cc.com>
2023-07-13 16:56:19 +0100

    Updated azapi id lookup

--- infrastructure/modules/logic-app/data.tf
+++ infrastructure/modules/logic-app/data.tf
@@ -3,12 +3,6 @@ data "azurerm_managed_api" "service_bus_managed_api" {
   name     = "servicebus"
 }
 
-data "azapi_resource" "zendesk_custom_api" {
-  parent_id = var.resource_group_id
-  name      = "zendesk-custom-api"
-  type      = "Microsoft.Web/customApis@2016-06-01"
-}
-
 data "azurerm_key_vault_secret" "zendesk_username" {
   name         = "zendesk-service-username"
   key_vault_id = var.key_vault_id
