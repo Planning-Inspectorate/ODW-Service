7e147a38c1f139dc4e67304fcfa219ef29be48e0
adelany <alex.delany@optima-cc.com>
2023-07-13 13:08:39 +0100

    Added custom template for api

--- infrastructure/modules/logic-app/data.tf
+++ infrastructure/modules/logic-app/data.tf
@@ -7,3 +7,9 @@ data "azurerm_managed_api" "service_bus_managed_api" {
   location = var.location
   name     = "servicebus"
 }
+
+data "azapi_resource" "zendesk_custom_api" {
+  parent_id = var.resource_group_id
+  name      = "zendesk-custom-api"
+  type      = "Microsoft.Web/customApis@2016-06-01"
+}
