d1f7e03cab71c9575b8bde3d10845f2f46c4e063
adelany <alex.delany@optima-cc.com>
2023-08-01 14:30:55 +0100

    Updated keyvault apim

--- infrastructure/workload-api-manangement.tf
+++ infrastructure/workload-api-manangement.tf
@@ -23,11 +23,13 @@ module "api_management" {
 
   environment         = var.environment
   resource_group_name = azurerm_resource_group.api_management[0].name
+  key_vault_id        = module.synapse_data_lake.key_vault_id
   location            = module.azure_region.location_cli
-  service_name        = local.service_name
   publisher_name      = var.apim_publisher_name
   publisher_email     = var.apim_publisher_email
+  service_name        = local.service_name
   sku_name            = var.apim_sku_name
+
 
   tags = local.tags
 }
@@ -39,10 +41,11 @@ module "api_management_failover" {
 
   environment         = var.environment
   resource_group_name = azurerm_resource_group.api_management_failover[0].name
+  key_vault_id         = module.synapse_data_lake_failover.key_vault_id
   location            = module.azure_region.paired_location.location_cli
-  service_name        = local.service_name
   publisher_name      = var.apim_publisher_name
   publisher_email     = var.apim_publisher_email
+  service_name        = local.service_name
   sku_name            = var.apim_sku_name
 
   tags = local.tags
