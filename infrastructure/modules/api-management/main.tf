83f5727db54b2eadd366be6f1ca4a1c58ea79422
adelany <alex.delany@optima-cc.com>
2023-08-04 11:35:25 +0100

    Updated apim networking

--- infrastructure/modules/api-management/main.tf
+++ infrastructure/modules/api-management/main.tf
@@ -7,10 +7,33 @@ resource "azurerm_api_management" "api_management" {
 
   sku_name = var.sku_name
 
-  public_network_access_enabled = false
+  #public_network_access_enabled = false
   virtual_network_configuration {
     subnet_id = var.synapse_vnet_subnet_names[var.synapse_apim_subnet_name]
   }
 
+  identity {
+    type = "SystemAssigned"
+  }
+
   tags = local.tags
 }
+
+# Create Application Insights
+# resource "azurerm_application_insights" "ai" {
+#   name                = local.appInsightsName
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   application_type    = "web"
+#   tags                = var.tags
+# }
+# # Create Logger
+# resource "azurerm_api_management_logger" "apimLogger" {
+#   name                = "${local.apimName}-logger"
+#   api_management_name = azurerm_api_management.apim.name
+#   resource_group_name = data.azurerm_resource_group.rg.name
+
+#   application_insights {
+#     instrumentation_key = azurerm_application_insights.ai.instrumentation_key
+#   }
+# }
