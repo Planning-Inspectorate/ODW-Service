2d2873e6867438d5595a798b0a962fe76651befd
adelany <alex.delany@optima-cc.com>
2023-09-28 11:23:25 +0100

    Updated variables

--- infrastructure/modules/api-management/apim-policy.tf
+++ infrastructure/modules/api-management/apim-policy.tf
@@ -1,7 +1,7 @@
 resource "azurerm_api_management_api_policy" "api_management_api_policy" {
   for_each = local.apim_api_policies
 
-  api_name            = each.key
+  api_name            = azurerm_api_management.api_management[each.key].name
   api_management_name = azurerm_api_management.api_management.name
   resource_group_name = var.resource_group_name
   xml_content         = file("${path.module}/policies/${each.value.xml_content}")
