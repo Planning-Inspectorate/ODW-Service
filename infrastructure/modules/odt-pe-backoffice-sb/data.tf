27efd8d50485df8f8e2bec67341a1d28720ef020
adelany <alex.delany@optima-cc.com>
2023-10-13 16:57:58 +0100

    Updated odt service-user

--- infrastructure/modules/odt-pe-backoffice-sb/data.tf
+++ infrastructure/modules/odt-pe-backoffice-sb/data.tf
@@ -6,7 +6,8 @@ data "azurerm_resources" "odt_pe_backoffice_sb" {
 }
 
 data "azurerm_servicebus_topic" "odt_backoffice_topic" {
-  provider     = azurerm.odt
-  name         = "service_user"
-  namespace_id = data.azurerm_resources.odt_pe_backoffice_sb.namespace.id
+  provider            = azurerm.odt
+  name                = "service-user"
+  namespace_name      = var.odt_back_office_service_bus_resource_group_name
+  resource_group_name = var.odt_back_office_service_bus_name
 }
