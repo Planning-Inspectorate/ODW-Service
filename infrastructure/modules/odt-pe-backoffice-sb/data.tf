2d4beccf587c77c51cad13a5920a74301b2d1a8c
adelany <alex.delany@optima-cc.com>
2023-10-13 17:08:16 +0100

    Updated

--- infrastructure/modules/odt-pe-backoffice-sb/data.tf
+++ infrastructure/modules/odt-pe-backoffice-sb/data.tf
@@ -8,6 +8,6 @@ data "azurerm_resources" "odt_pe_backoffice_sb" {
 data "azurerm_servicebus_topic" "odt_backoffice_topic" {
   provider            = azurerm.odt
   name                = "service-user"
-  namespace_name      = var.odt_back_office_service_bus_resource_group_name
-  resource_group_name = var.odt_back_office_service_bus_name
+  namespace_name      = var.odt_back_office_service_bus_name
+  resource_group_name = var.odt_back_office_service_bus_resource_group_name
 }
