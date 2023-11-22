9e3fe097e15a06c3796f77624706419259d96e70
adelany <alex.delany@optima-cc.com>
2023-11-22 17:09:46 +0000

    Updated

--- infrastructure/modules/function-app/variables.tf
+++ infrastructure/modules/function-app/variables.tf
@@ -138,7 +138,7 @@ variable "site_config_defaults" {
     websockets_enabled          = true
     vnet_route_all_enabled      = false
     application_stack = {
-      python_version = "3.10"
+      powershell_core_version = "7.0"
     }
     ip_restrictions = {
       ip_addresses = []
