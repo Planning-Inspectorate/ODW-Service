00830b3bc2cfb7f6e72e6997c024425e69ea6f43
adelany <alex.delany@optima-cc.com>
2023-11-23 17:01:59 +0000

    Updated provider

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -10,10 +10,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.76.0"
+      version = "~> 3.56.0"
     }
   }
-  required_version = ">= 1.1.6, < 3.76.0"
+  required_version = ">= 1.1.6, < 3.56.0"
 }
 
 provider "azurerm" {
