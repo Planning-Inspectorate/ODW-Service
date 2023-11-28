f6595deea9b90b8394153e3d243cdf0f6372635e
adelany <alex.delany@optima-cc.com>
2023-11-28 16:50:13 +0000

    Update subnets output

--- infrastructure/modules/synapse-network/locals.tf
+++ infrastructure/modules/synapse-network/locals.tf
@@ -9,13 +9,7 @@ locals {
 
   nsg_path = "providers/Microsoft.Network/networkSecurityGroups"
 
-  subnets = { for k, v in var.vnet_subnets :
-    k => {
-      cidr_subnet_offset = v.new_bits
-      name               = v.name
-      service_delegation = v.service_delegation
-    }
-  }
+  subnets = { for k, v in module.subnets.network_cidr_blocks : k => merge(var.var.vnet_subnets[k], { service_delegation = v.service_delegation }) }
 
   tags = merge(
     var.tags,
