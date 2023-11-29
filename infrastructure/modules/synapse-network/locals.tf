1cff2d5520b819407ae04fd85e58663beb8a29f2
adelany <alex.delany@optima-cc.com>
2023-11-29 01:23:53 +0000

    Updated

--- infrastructure/modules/synapse-network/locals.tf
+++ infrastructure/modules/synapse-network/locals.tf
@@ -8,6 +8,7 @@ locals {
     for subnet in var.vnet_subnets : subnet.name => {
       new_bits           = subnet.new_bits
       cidr_block         = module.subnets.network_cidr_blocks[subnet.name]
+      service_endpoints  = subnet.service_endpoints
       service_delegation = subnet.service_delegation
     }
   }
