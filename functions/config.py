cf4ad6b13b3dbaebfe7e0d0aa6c0bb80f7bc9c66
Chris Topping <chris.topping@kinandcarta.com>
2023-12-04 09:27:32 +0000

    changing nsip topic and subscription to correct name

--- functions/config.py
+++ functions/config.py
@@ -11,8 +11,8 @@ ODT_NAMESPACE = "pins-sb-back-office-dev-ukw-001.servicebus.windows.net"
 ODW_NAMESPACE = "pins-sb-odw-dev-uks-b9rt9m.servicebus.windows.net"
 SERVICE_USER_SUBSCRIPTION = "service-user"
 SERVICE_USER_TOPIC = "service-user"
-NSIP_SUBSCRIPTION = "nsip"
-NSIP_TOPIC = "nsip"
+NSIP_SUBSCRIPTION = "nsip-project"
+NSIP_TOPIC = "nsip-project"
 MAX_MESSAGE_COUNT = 10
 UTC_TIMESTAMP = (
     datetime.datetime.now(datetime.timezone.utc)
