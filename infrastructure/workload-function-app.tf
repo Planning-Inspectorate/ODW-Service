6be8c6f6c52c11e1978b8dd1770471e0fe626262
adelany <alex.delany@optima-cc.com>
2023-12-05 14:26:45 +0000

    Updated function syntax

--- infrastructure/workload-function-app.tf
+++ infrastructure/workload-function-app.tf
@@ -45,11 +45,11 @@ module "storage_account" {
 
   source = "./modules/storage-account"
 
-  resource_group_name                     = azurerm_resource_group.function_app[0].name
-  service_name                            = local.service_name
-  environment                             = var.environment
-  location                                = module.azure_region.location_cli
-  tags                                    = local.tags
+  resource_group_name = azurerm_resource_group.function_app[0].name
+  service_name        = local.service_name
+  environment         = var.environment
+  location            = module.azure_region.location_cli
+  tags                = local.tags
   #network_rule_virtual_network_subnet_ids = [module.synapse_network.vnet_subnets[local.functionapp_subnet_name]]
 }
 
@@ -58,11 +58,11 @@ module "storage_account_failover" {
 
   source = "./modules/storage-account"
 
-  resource_group_name                     = azurerm_resource_group.function_app_failover[0].name
-  service_name                            = local.service_name
-  environment                             = var.environment
-  location                                = module.azure_region.paired_location.location_cli
-  tags                                    = local.tags
+  resource_group_name = azurerm_resource_group.function_app_failover[0].name
+  service_name        = local.service_name
+  environment         = var.environment
+  location            = module.azure_region.paired_location.location_cli
+  tags                = local.tags
   #network_rule_virtual_network_subnet_ids = [module.synapse_network_failover.vnet_subnets[local.functionapp_subnet_name]]
 }
 
