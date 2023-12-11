a40cd707e8f4cd6d6542684afa78c30d318b9574
Chris Topping <chris.topping@kinandcarta.com>
2023-12-11 16:19:33 +0000

    refactored function app code to use json schemas from ODT

---
+++ functions/validation/validation_service_user.py
@@ -0,0 +1,45 @@
+import sys
+from pathlib import Path
+current_file = Path(__file__).resolve()
+functions_folder = current_file.parent.parent
+sys.path.append(str(functions_folder))
+
+from models import service_user
+
+from pydantic import BaseModel, ValidationError
+from servicebus_funcs import get_messages
+import config
+
+_NAMESPACE = config.ODT_NAMESPACE
+_SUBSCRIPTION = config.NSIP_PROJECT_SUBSCRIPTION
+_TOPIC = config.NSIP_PROJECT_TOPIC
+_MAX_MESSAGE_COUNT = config.MAX_MESSAGE_COUNT
+_CREDENTIAL = config.CREDENTIAL
+_MESSAGES = service_user.ServiceUser
+
+def validate():
+    class MessageInstances(BaseModel):
+
+        """
+        Represents a pydantic model for a list of ServiceUser instances.
+
+        Args:
+            messagedata (list[ServiceUser]): The list of ServiceUser instances.
+
+        Attributes:
+            messagedata (list[ServiceUser]): The list of ServiceUser instances.
+        """
+
+        messagedata: list[_MESSAGES]
+
+    _data = get_messages(
+        _NAMESPACE, _CREDENTIAL, _TOPIC, _SUBSCRIPTION, _MAX_MESSAGE_COUNT
+    )
+
+    try:
+        MessageInstances(messagedata=_data)
+        print("VALIDATION SUCCEEDED!")
+        print(f"{len(_data)} MESSAGES PROCESSED")
+    except ValidationError as e:
+        print(e)
+        raise e
