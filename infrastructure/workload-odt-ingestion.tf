0a7d76a7152d74fc2acc2a0a6caff34e26572257
Christopher Daly <96062341+chrisdalykinandcarta@users.noreply.github.com>
2023-12-13 15:55:42 +0000

    Try accessing element directly (#926)
    * Try accessing element directly
    
    * Is identity a list of lists?

--- infrastructure/workload-odt-ingestion.tf
+++ infrastructure/workload-odt-ingestion.tf
@@ -30,6 +30,7 @@ resource "azurerm_role_assignment" "servicebus_data_receiver" {
   scope                = each.value
   role_definition_name = "Azure Service Bus Data Receiver"
   # TODO: Why is the output a list?
-  principal_id = one(module.function_app).identity[0].principal_id
+  principal_id = var.odt_back_office_service_bus_enabled ? module.odt_ingestion_function_app[0].identity[0][0].principal_id : null
+
 }
 
