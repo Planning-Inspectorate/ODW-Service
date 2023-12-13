4fd7ea0f197190dd2dfda9c8025a103baae19d9e
adelany <alex.delany@optima-cc.com>
2023-12-13 11:24:28 +0000

    Add perms to data lake storage for function app

--- infrastructure/modules/synapse-data-lake/data.tf
+++ infrastructure/modules/synapse-data-lake/data.tf
@@ -1,1 +1,6 @@
 data "azurerm_client_config" "current" {}
+
+data "azurerm_function_app" "function_app" {
+  name                = var.function_app_name
+  resource_group_name = var.function_app_resource_group_name
+}
