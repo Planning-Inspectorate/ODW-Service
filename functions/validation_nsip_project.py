c087a264c84fa70d26667747ae50589cda4b79a0
ChrisToppingKandC <137051790+ChrisToppingKandC@users.noreply.github.com>
2023-12-13 14:53:43 +0000

    Feat/model from json (#922)
    * refactored function app code to use json schemas from ODT
    
    * more code refactoring
    
    * added to config file and set variables to Dev for testing
    
    * small change to file path in config file
    
    * changed folder structure
    
    * added return value to validate function

---
+++ functions/validation_nsip_project.py
@@ -0,0 +1,50 @@
+"""
+Module containing a validate function to be called to run validation
+of a list of sevricebus messages
+"""
+
+import model_nsip_project
+
+from pydantic import BaseModel, ValidationError
+from servicebus_funcs import get_messages
+import config
+
+_NAMESPACE = config.ODW_NAMESPACE_DEV
+_SUBSCRIPTION = config.NSIP_PROJECT_SUBSCRIPTION
+_TOPIC = config.NSIP_PROJECT_TOPIC
+_CREDENTIAL = config.CREDENTIAL
+_MESSAGES = model_nsip_project.NsipProject
+_MAX_MESSAGE_COUNT = config.MAX_MESSAGE_COUNT
+
+
+def validate() -> list:
+    """
+    Function to validate a list of servicebus messages
+    """
+
+    _data = get_messages(
+    _NAMESPACE, _CREDENTIAL, _TOPIC, _SUBSCRIPTION, _MAX_MESSAGE_COUNT
+    )   
+    
+    class MessageInstances(BaseModel):
+
+        """
+        Represents a pydantic model for a list of ServiceUser instances.
+
+        Args:
+            messagedata (list[ServiceUser]): The list of ServiceUser instances.
+
+        Attributes:
+            messagedata (list[ServiceUser]): The list of ServiceUser instances.
+        """
+
+        messagedata: list[_MESSAGES]
+
+    try:
+        MessageInstances(messagedata=_data)
+        print("VALIDATION SUCCEEDED!")
+        print(f"{len(_data)} MESSAGES PROCESSED")
+        return _data
+    except ValidationError as e:
+        print(e)
+        raise e
