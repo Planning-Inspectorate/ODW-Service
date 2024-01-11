88e9bcf79ef4f282251d5d56b1057e48db2f1c64
Chris Topping <chris.topping@kinandcarta.com>
2024-01-11 11:24:32 +0000

    added sample code to fit synapse data to data model schema for validation

--- scripts/odw-validation/nsip-exam-timetable.py
+++ scripts/odw-validation/nsip-exam-timetable.py
@@ -4,11 +4,15 @@ import pprint
 import model_nsip_exam_timetable
 from pydantic import BaseModel, ValidationError
 
+# create a list of fields that sit in the events nested array. Taken directly from pydantic model Event class
 _event_fields = dict(model_nsip_exam_timetable.Event.__annotations__.items())
+# convert to a dictionary
 _event_dict = {k.lower(): v for k, v in _event_fields.items()}
-
+# list the keys that need to be excluded form the top level,
+# i.e. if a field is in a nested array it shouldn't be in the top level as well
 _excluded_fields = [k.lower() for k, v in _event_dict.items()]
 
+# sample file for testing
 _file_name = r"C:\Users\ChrisTopping\Downloads\nsip-exam-timetable-sample.csv"
 
 _data = []
@@ -23,11 +27,13 @@ for row in _data:
     _lowercase_row = {k.lower(): v for k, v in row.items()}
     _caseReference = _lowercase_row["casereference"]
     if _caseReference not in _merged_data:
+        # set casereference as the primary key and add all other fields to top level
         _merged_data[_caseReference] = {
             k: v for k, v in _lowercase_row.items() if k.lower() not in _excluded_fields
         }
+        # create empty nested array for events
         _merged_data[_caseReference]["events"] = []
-
+    # populate events array
     _events = _merged_data[_caseReference]["events"]
     _new_event = {k.lower(): _lowercase_row[k] for k, v in _event_dict.items()}
     if _new_event not in _events:
@@ -36,14 +42,13 @@ for row in _data:
 _final_data = list(_merged_data.values())
 
 
+# validation of data against model
+
 class ExaminationTimetableInstances(BaseModel):
     data: list[model_nsip_exam_timetable.ExaminationTimetable]
-
-
-pprint.pprint(_final_data)
 
 try:
     ExaminationTimetableInstances(data=_final_data)
 except ValidationError as e:
     print(e)
-    # pprint.pprint(e.errors())
+    pprint.pprint(e.errors())
