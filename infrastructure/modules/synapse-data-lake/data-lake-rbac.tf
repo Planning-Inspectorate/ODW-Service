b8e6acb4cae8abe0461630a03381e6fa9ce2ef99
Alex Delany <alex.delany@planninginspectorate.gov.uk>
2024-01-30 19:58:54 +0000

    Updated function app ids

--- infrastructure/modules/synapse-data-lake/data-lake-rbac.tf
+++ infrastructure/modules/synapse-data-lake/data-lake-rbac.tf
@@ -16,10 +16,9 @@ resource "azurerm_role_assignment" "terraform" {
 
 resource "azurerm_role_assignment" "function_app" {
   for_each = var.function_app_identity != null ? {
-    for identity in var.function_app_identity : identity.principal_id => identity
+    for identity in var.function_app_identity : identity => identity
   } : {}
 
   scope                = azurerm_storage_account.synapse.id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = each.value.principal_id
-}
+  principal_id         = each.value
