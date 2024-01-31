6da75acc0d5eb9c9d1fadb8019df69a6e705e260
Alex Delany <alex.delany@planninginspectorate.gov.uk>
2024-01-31 21:51:53 +0000

    Updated

--- infrastructure/workload-function-app.tf
+++ infrastructure/workload-function-app.tf
@@ -135,5 +135,5 @@ resource "azurerm_role_assignment" "servicebus_receiver" {
 
   scope                = each.value
   role_definition_name = "Azure Service Bus Data Receiver"
-  principal_id         = module.function_app[*].identity.principal_id
+  principal_id         = module.function_app[fnapp01].identity.principal_id
 }
