6b3cd24faad01c3587876de75ca3e843ac6c9b4a
Alex Delany <alex.delany@planninginspectorate.gov.uk>
2024-01-31 13:13:28 +0000

    Updated outputs

--- infrastructure/modules/function-app/output.tf
+++ infrastructure/modules/function-app/output.tf
@@ -25,5 +25,5 @@ output "identity" {
 
 output "principal_ids" {
   description = "List of principal IDs"
-  value       = azurerm_linux_function_app.function.identity[0].principal_ids
+  value       = azurerm_linux_function_app.function.identity[0].principal_id
 }
