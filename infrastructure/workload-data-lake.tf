724d4938f300abeb7468470788a3d4aa0806cd8f
Alex Delany <alex.delany@planninginspectorate.gov.uk>
2024-01-31 22:04:15 +0000

    Updated function app

--- infrastructure/workload-data-lake.tf
+++ infrastructure/workload-data-lake.tf
@@ -32,7 +32,7 @@ module "synapse_data_lake" {
   data_lake_storage_containers           = var.data_lake_storage_containers
   devops_agent_subnet_name               = module.synapse_network.devops_agent_subnet_name
   firewall_allowed_ip_addresses          = yamldecode(file(local.firewall_config_file_path))
-  function_app_principal_id              = module.function_app[*].identity.principal_id
+  function_app_principal_id              = module.function_app[0].identity[0].principal_id
   key_vault_role_assignments             = var.key_vault_role_assignments
   network_resource_group_name            = azurerm_resource_group.network.name
   synapse_private_endpoint_subnet_name   = module.synapse_network.synapse_private_endpoint_subnet_name
@@ -64,7 +64,7 @@ module "synapse_data_lake_failover" {
   data_lake_storage_containers           = var.data_lake_storage_containers
   devops_agent_subnet_name               = module.synapse_network_failover.devops_agent_subnet_name
   firewall_allowed_ip_addresses          = yamldecode(file(local.firewall_config_file_path))
-  function_app_principal_id              = try(module.function_app_failover[*].identity.principal_id, [])
+  function_app_principal_id              = var.failover_deployment ? module.function_app_failover[0].identity[0].principal_id : module.function_app[0].identity[0].principal_id
   key_vault_role_assignments             = var.key_vault_role_assignments
   network_resource_group_name            = azurerm_resource_group.network_failover.name
   synapse_private_endpoint_subnet_name   = module.synapse_network_failover.synapse_private_endpoint_subnet_name
