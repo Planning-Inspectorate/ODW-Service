976183fdb50873909ebc99813995f549385104d2
Alex Delany <alex.delany@planninginspectorate.gov.uk>
2024-02-01 17:28:30 +0000

    Updated outputs

--- infrastructure/workload-monitoring.tf
+++ infrastructure/workload-monitoring.tf
@@ -29,7 +29,6 @@ module "synapse_monitoring" {
   alert_scope_service_health               = var.alert_scope_service_health
   alert_threshold_data_lake_capacity_bytes = var.alert_threshold_data_lake_capacity_bytes
   data_lake_account_id                     = module.synapse_data_lake.data_lake_account_id
-  function_app_id                          = module.function_app[*].function_app_id
   key_vault_id                             = module.synapse_data_lake.key_vault_id
   service_bus_namespace_id                 = module.synapse_ingestion.service_bus_namespace_id
   spark_pool_enabled                       = var.spark_pool_enabled
@@ -59,8 +58,7 @@ module "synapse_monitoring_failover" {
   alert_scope_service_health               = var.alert_scope_service_health
   alert_threshold_data_lake_capacity_bytes = var.alert_threshold_data_lake_capacity_bytes
   data_lake_account_id                     = module.synapse_data_lake_failover.data_lake_account_id
-  key_vault_id                             = module.synapse_data_lake_failover.key_vault_id
-  function_app_id                          = module.function_app_failover[*].function_app_id
+  key_vault_id                             = module.synapse_data_lake_failover.key_vault_idid
   service_bus_namespace_id                 = module.synapse_ingestion_failover[0].service_bus_namespace_id
   spark_pool_enabled                       = var.spark_pool_enabled
   sql_pool_enabled                         = var.sql_pool_enabled
