c334b5092d4f718be01069a179c23f336c4aa71a
Alex Delany <alex.delany@planninginspectorate.gov.uk>
2024-02-01 19:49:24 +0000

    Updated data lake syntax

--- infrastructure/workload-data-lake.tf
+++ infrastructure/workload-data-lake.tf
@@ -64,13 +64,13 @@ module "synapse_data_lake_failover" {
   data_lake_storage_containers           = var.data_lake_storage_containers
   devops_agent_subnet_name               = module.synapse_network_failover.devops_agent_subnet_name
   firewall_allowed_ip_addresses          = yamldecode(file(local.firewall_config_file_path))
-  #function_app_principal_id              = try(module.function_app_failover[*], {})
-  key_vault_role_assignments           = var.key_vault_role_assignments
-  network_resource_group_name          = azurerm_resource_group.network_failover.name
-  synapse_private_endpoint_subnet_name = module.synapse_network_failover.synapse_private_endpoint_subnet_name
-  tenant_id                            = var.tenant_id
-  vnet_subnet_ids                      = module.synapse_network_failover.vnet_subnets
-  vnet_subnet_ids_failover             = module.synapse_network.vnet_subnets
+  function_app_principal_ids             = try(module.function_app, {})
+  key_vault_role_assignments             = var.key_vault_role_assignments
+  network_resource_group_name            = azurerm_resource_group.network_failover.name
+  synapse_private_endpoint_subnet_name   = module.synapse_network_failover.synapse_private_endpoint_subnet_name
+  tenant_id                              = var.tenant_id
+  vnet_subnet_ids                        = module.synapse_network_failover.vnet_subnets
+  vnet_subnet_ids_failover               = module.synapse_network.vnet_subnets
 
   tags = local.tags
 }
