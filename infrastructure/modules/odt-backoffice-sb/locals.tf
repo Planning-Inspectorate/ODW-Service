1f493c906e1e6e5b0cab5086dd00625ed97b6003
Alex Delany <alex.delany@planninginspectorate.gov.uk>
2024-02-02 18:13:09 +0000

    Updated

--- infrastructure/modules/odt-backoffice-sb/locals.tf
+++ infrastructure/modules/odt-backoffice-sb/locals.tf
@@ -89,7 +89,7 @@ locals {
     for function_app, principal_id in var.function_app_principal_ids : [
       for subscription in local.odt_backoffice_sb_subscriptions : {
         function_app_name = function_app
-        subscription_name = subscription.value.subscription_name
+        subscription_name = subscription
         principal_id      = principal_id
       }
     ]
