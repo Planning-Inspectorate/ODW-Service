35fa7e79cd2d017618b4f97eb9bb7215fc2133f2
Alex Delany <alex.delany@planninginspectorate.gov.uk>
2024-02-02 11:08:00 +0000

    Updated

--- infrastructure/modules/synapse-data-lake/data-lake-rbac.tf
+++ infrastructure/modules/synapse-data-lake/data-lake-rbac.tf
@@ -16,7 +16,7 @@ resource "azurerm_role_assignment" "terraform" {
 
 resource "azurerm_role_assignment" "function_app" {
   for_each = {
-    for identity in var.function_app_principal_ids : identity.object_id => identity
+    for principal_id, identity in var.function_app_principal_ids : principal_id => identity
   }
 
   scope                = azurerm_storage_account.synapse.id
