a4f2640c1aaf9427057437c3a84e5c0d3fbb25b0
Alex Delany <alex.delany@planninginspectorate.gov.uk>
2024-02-06 12:51:16 +0000

    Updated function app

--- infrastructure/workload-function-app.tf
+++ infrastructure/workload-function-app.tf
@@ -133,7 +133,7 @@ module "function_app_failover" {
 resource "azurerm_role_assignment" "servicebus_receiver" {
   for_each = {
     for function_app in var.function_app : function_app.name => {
-      for k, v in module.odt_backoffice_sb.subscription_ids : k => {
+      for k, v in one(module.odt_backoffice_sb).subscription_ids : k => {
         subscription_id = v
       }
     }
