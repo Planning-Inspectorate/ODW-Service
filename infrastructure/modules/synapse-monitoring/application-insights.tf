3b7034ca47474461051f71118cff120a21d56f45
Alex Delany <alex.delany@planninginspectorate.gov.uk>
2024-02-07 13:55:41 +0000

    Updated

--- infrastructure/modules/synapse-monitoring/application-insights.tf
+++ infrastructure/modules/synapse-monitoring/application-insights.tf
@@ -9,15 +9,15 @@ resource "azurerm_application_insights" "synapse" {
   tags = local.tags
 }
 
-resource "azurerm_application_insights" "function_app_insights {
-  for_each = {
-    for function_app in var.function_app : function_app.name => function_app if var.function_app_enabled == true
-  }
-
-  name                = pins-appi-[each.key]-${local.resource_suffix}
+resource "azurerm_application_insights" "function_app_insights" {
+  for_each = var.function_app_identity
+  
+  name                = "pins-appi-${each.key}-${local.resource_suffix}"
   location            = var.location
   resource_group_name = var.resource_group_name
   application_type    = "web"
+  retention_in_days   = var.log_retention_days
+  workspace_id        = azurerm_log_analytics_workspace.synapse.id
 
   tags = local.tags
 }
--- infrastructure/modules/synapse-monitoring/monitor-function-app.tf
+++ infrastructure/modules/synapse-monitoring/monitor-function-app.tf
@@ -1,10 +1,8 @@
 resource "azurerm_monitor_diagnostic_setting" "function_app" {
-  for_each = {
-    for function_app in var.function_app : function_app.name => function_app if var.function_app_enabled == true
-  }
+  for_each = var.function_app_identity
 
   name                       = "Function App Logs - ${each.key}"
-  target_resource_id         = module.function_app[each.key].id
+  target_resource_id         = each.value
   log_analytics_workspace_id = azurerm_log_analytics_workspace.synapse.id
 
 
