06d374a6b26d11de6f0afa39a7540aec5c3ee79d
Alex Delany <alex.delany@planninginspectorate.gov.uk>
2024-03-22 11:45:19 +0000

    Remove odt sb subscription rbac

--- infrastructure/workload-function-app.tf
+++ infrastructure/workload-function-app.tf
@@ -132,20 +132,6 @@ module "function_app_failover" {
   servicebus_namespace       = var.odt_back_office_service_bus_name
 }
 
-# resource "azurerm_role_assignment" "odt_servicebus_receiver" {
-#   for_each = {
-#     for function in local.function_app_subscriptions : "${function.name}.${function.subscription_ids}" => function if var.function_app_enabled == true
-#   }
-
-#   scope                = each.value.subscription_ids
-#   role_definition_name = "Azure Service Bus Data Receiver"
-#   principal_id         = module.function_app[each.value.name].identity[0].principal_id
-
-#   depends_on = [
-#     module.odt_backoffice_sb
-#   ]
-# }
-
 resource "azurerm_role_assignment" "odt_servicebus_namespace" {
   for_each = {
     for function_app in var.function_app : function_app.name => function_app if var.function_app_enabled == true
@@ -154,10 +140,6 @@ resource "azurerm_role_assignment" "odt_servicebus_namespace" {
   scope                = module.odt_backoffice_sb[0].namespace_id
   role_definition_name = "Azure Service Bus Data receiver"
   principal_id         = module.function_app[each.key].identity[0].principal_id
-
-  depends_on = [
-    module.odt_backoffice_sb
-  ]
 }
 
 resource "azurerm_application_insights" "function_app_insights" {
