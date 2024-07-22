ecefaaa2ec5094c58e738dded6713d704872a7ce
Kathryn Thompson <kathryn.thompson@kinandcarta.com>
2024-07-22 17:30:18 +0100

    Update test_entraid.py

--- workspace/testing/tests/test_entraid.py
+++ workspace/testing/tests/test_entraid.py
@@ -1,6 +1,16 @@
 import pytest
 import json
 
+""" 
+Things to do (based on py_unit_tests)
+
+compare schemas between data from orchestration.json and standardised
+compare schemas between data from orchestration.json and harmonised
+compare counts between standardised and harmonised (they should be identical)
+compare the curated and harmonised data, check that all active=Y exist in harmonised
+ """
+
+
 def test_source_to_processed_workflow():
     print("Running Test")
     assert True is True
