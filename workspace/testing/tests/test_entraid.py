2a0a0e13817751171c48cff45f35856d689e31cd
Kathryn Thompson <kathryn.thompson@kinandcarta.com>
2024-07-26 14:27:05 +0100

    Update test_entraid.py

--- workspace/testing/tests/test_entraid.py
+++ workspace/testing/tests/test_entraid.py
@@ -37,7 +37,7 @@ def test_entraid_notebook(credential_name, azure_credential, synapse_endpoint: s
     warnings.filterwarnings("ignore", category=DeprecationWarning) 
 
     # run the testing notebook
-    notebookname: str = "utils/unit-tests/py_unit_tests"
+    notebookname: str = "py_unit_tests"
     
     # Trigger the Master Pipeline for Landing to Raw Zone
     notebook_raw_params = {
