c49d3c4310e6e9c66626ee8cb4543d0aae8943f3
Kathryn Thompson <kathryn.thompson@kinandcarta.com>
2024-07-30 09:48:54 +0100

    Exit code fixing

--- workspace/testing/tests/test_entraid.py
+++ workspace/testing/tests/test_entraid.py
@@ -89,9 +89,9 @@ def test_entraid_notebook(credential_name, azure_credential, synapse_endpoint: s
 
     #run the notebook
     (notebook_run_result, exitMessage) = pipelineutils.run_and_observe_notebook(credential_name, azure_credential, synapse_endpoint, notebookname, notebook_raw_params)
-    print("Notebook response *" +exitMessage +"*")
-    assert notebook_run_result == constants.NOTEBOOK_SUCCESS_STATUS
-
+    print("Notebook response *" +str(exitMessage) +"*")
+    assert notebook_run_result == constants.NOTEBOOK_SUCCESS_STATUS 
+    assert exitMessage == constants.NOTEBOOK_EXIT_CODE_SUCCESS
     print("test_entraid Completed")
 
 @pytest.fixture(autouse=True)
