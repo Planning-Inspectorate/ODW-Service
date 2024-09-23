1270ba5749abde3e48ca66789a971d88c984418b
Chris Topping <chris.topping@kinandcarta.com>
2024-09-23 10:55:41 +0100

    ran terraform fmt

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -27,14 +27,14 @@ provider "azurerm" {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.odt_subscription_id
-  alias           = "odt"
+  subscription_id            = var.odt_subscription_id
+  alias                      = "odt"
   skip_provider_registration = true
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = var.horizon_subscription_id
-  alias           = "horizon"
+  subscription_id            = var.horizon_subscription_id
+  alias                      = "horizon"
   skip_provider_registration = true
 }
