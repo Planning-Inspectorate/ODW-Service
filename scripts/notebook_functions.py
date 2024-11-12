2787fe7df2a119e75f2973dcdb8c71122d1d34d1
Chris Topping <chris.topping@kinandcarta.com>
2024-11-12 15:45:24 +0000

    ran ruff and sourcery checks

--- scripts/notebook_functions.py
+++ scripts/notebook_functions.py
@@ -1,18 +1,21 @@
+"""
+Functions to interact with Azure Synapse
+"""
 from azure.identity import DefaultAzureCredential
 import requests
 import pprint
 import os
 import json
 
-CREDENTIAL = DefaultAzureCredential()
-API_VERSION = '2020-12-01'
-SYNAPSE_ENDPOINT = 'https://pins-synw-odw-dev-uks.dev.azuresynapse.net/notebooks/'
-NOTEBOOK = 'ci_cd_test_notebook'
+_CREDENTIAL = DefaultAzureCredential()
+_API_VERSION = '2020-12-01'
+_SYNAPSE_ENDPOINT = 'https://pins-synw-odw-dev-uks.dev.azuresynapse.net/notebooks/'
+_NOTEBOOK = 'ci_cd_test_notebook'
 
-token = CREDENTIAL.get_token("https://dev.azuresynapse.net/.default").token
+_token = _CREDENTIAL.get_token("https://dev.azuresynapse.net/.default").token
 
-headers = {
-    "Authorization": f"Bearer {token}",
+_headers = {
+    "Authorization": f"Bearer {_token}",
     "Content-Type": "application/json"
 }
 
@@ -20,44 +23,42 @@ def delete_notebook(notebook: str) -> dict:
     """
     Deletes a notebook from Synapse Live Mode in the target workspace
     """
-    notebook_url = f'{SYNAPSE_ENDPOINT}{notebook}?api-version={API_VERSION}'
-    response = requests.delete(notebook_url, headers=headers)
-    data = response.json()
-    return data
+    notebook_url = f'{_SYNAPSE_ENDPOINT}{notebook}?api-version={_API_VERSION}'
+    response = requests.delete(notebook_url, headers=_headers)
+    return response.json()
 
 def get_notebook(notebook: str) -> dict:
     """
     Gets a notebook from Synapse Live Mode in the target workspace
     """
-    notebook_url = f'{SYNAPSE_ENDPOINT}{notebook}?api-version={API_VERSION}'
-    response = requests.get(notebook_url, headers=headers)
-    data = response.json()
-    return data
+    notebook_url = f'{_SYNAPSE_ENDPOINT}{notebook}?api-version={_API_VERSION}'
+    response = requests.get(notebook_url, headers=_headers)
+    return response.json()
 
 def create_or_update_notebook(notebook: str) -> dict:
-
-    
     """
     This currently doesn't work.
     Error: Notebook cells must be defined in the payload
     """
-    notebook_url = f'{SYNAPSE_ENDPOINT}{notebook}?api-version={API_VERSION}'
+    notebook_url = f'{_SYNAPSE_ENDPOINT}{notebook}?api-version={_API_VERSION}'
     script_dir = os.path.dirname(__file__)
-    notebook_path = os.path.join(script_dir, f"../workspace/notebook/{NOTEBOOK}.json")
+    notebook_path = os.path.join(script_dir, f"../workspace/notebook/{_NOTEBOOK}.json")
     with open(notebook_path, 'r') as file:
         notebook_data = json.load(file)
 
     request_body = {
-        "name": NOTEBOOK,
+        "name": _NOTEBOOK,
         "properties": notebook_data
     }
 
-    response = requests.put(notebook_url, headers=headers, json=request_body)
-    data = response.json()
-    return data
+    response = requests.put(notebook_url, headers=_headers, json=request_body)
+    return response.json()
 
-def main():
-    pprint.pprint(get_notebook(notebook=NOTEBOOK))
+def main() -> dict:
+    """
+    Main function
+    """
+    pprint.pprint(get_notebook(notebook=_NOTEBOOK))
 
 if __name__ == '__main__':
     main()
