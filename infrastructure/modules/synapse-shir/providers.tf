95e668ee480d99e239f14f42d1b0c604026d9e88
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2024-12-17 11:00:06 +0000

    chore: updating azurerm to 4.13

--- infrastructure/modules/synapse-shir/providers.tf
+++ infrastructure/modules/synapse-shir/providers.tf
@@ -1,20 +1,10 @@
 terraform {
   required_providers {
     azurerm = {
-      source                = "hashicorp/azurerm"
-      version               = ">= 3.0, < 5.0"
-      configuration_aliases = [azurerm, azurerm.odt]
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0, < 5.0"
+      # configuration_aliases = [azurerm, azurerm.odt]
     }
   }
   required_version = ">= 1.5.7, < 1.10.0"
-}
-
-provider "azurerm" {
-  alias = "default"
-  features {}
-}
-
-provider "azurerm" {
-  alias = "odt"
-  features {}
 }
