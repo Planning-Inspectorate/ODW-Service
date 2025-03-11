613b04f32af7655b279d32fd336d5c31e0021046
stef-hmcts <stefania.deligia@hmcts.net>
2025-03-11 17:48:55 +0000

    Adjusted db name

--- functions/function_app.py
+++ functions/function_app.py
@@ -847,52 +847,35 @@ def getDaRT(req: func.HttpRequest, dart: func.SqlRowList) -> func.HttpResponse:
     except Exception as e:
         return func.HttpResponse(f"Unknown error: {str(e)}", status_code=500)
 
-@_app.function_name("testFunction")
+@_app.function_name(name="testFunction")
 @_app.route(route="testFunction", methods=["get"], auth_level=func.AuthLevel.FUNCTION)
-def test_function(req: func.HttpRequest) -> func.HttpResponse:
-    """
-    Azure Function endpoint for querying the tables_logs table.
-
-    Args:
-        req: An instance of `func.HttpRequest` representing the HTTP request.
-
-    Returns:
-        An instance of `func.HttpResponse` representing the HTTP response.
-    """
-
+@_app.sql_input(
+    arg_name="logs",
+    command_text="""
+    SELECT TOP (1000) [file_ID],
+                    [ingested_datetime],
+                    [ingested_by_process_name],
+                    [input_file],
+                    [modified_datetime],
+                    [modified_by_process_name],
+                    [entity_name],
+                    [rows_raw],
+                    [rows_new]
+    FROM logging.dbo.tables_logs
+    """,
+    command_type="Text",
+    connection_string_setting="SqlConnectionString"
+)
+def test_function(req: func.HttpRequest, logs: func.SqlRowList) -> func.HttpResponse:
     try:
-        _SCHEMA = _SCHEMAS["tables-logs.schema.json"]
-        _TOPIC = config["global"]["entities"]["tables-logs"]["topic"]
-        _SUBSCRIPTION = config["global"]["entities"]["tables-logs"]["subscription"]
-
-        _data = get_messages_and_validate(
-            namespace=_NAMESPACE,
-            credential=_CREDENTIAL,
-            topic=_TOPIC,
-            subscription=_SUBSCRIPTION,
-            max_message_count=_MAX_MESSAGE_COUNT,
-            max_wait_time=_MAX_WAIT_TIME,
-            schema=_SCHEMA,
-        )
-
-        _message_count = send_to_storage(
-            account_url=_STORAGE,
-            credential=_CREDENTIAL,
-            container=_CONTAINER,
-            entity="tables-logs",
-            data=_data,
-        )
-
-        response = json.dumps({"message": f"{_SUCCESS_RESPONSE} - {_message_count} messages sent to storage", "count": _message_count})
+        rows = []
+        for r in logs:
+            rows.append(json.loads(r.to_json()))
 
         return func.HttpResponse(
-            response,
-            status_code=200
+            json.dumps(rows),
+            status_code=200,
+            mimetype="application/json"
         )
-
     except Exception as e:
-        return (
-            func.HttpResponse(f"Validation error: {str(e)}", status_code=500)
-            if f"{_VALIDATION_ERROR}" in str(e)
-            else func.HttpResponse(f"Unknown error: {str(e)}", status_code=500)
-        )
+        return func.HttpResponse(f"Unknown error: {str(e)}", status_code=500)
