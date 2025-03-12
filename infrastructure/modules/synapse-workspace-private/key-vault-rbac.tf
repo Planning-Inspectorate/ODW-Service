ddea6c9cf73f6f37fb1e8571b036a77a536261a1
Chris Topping <chris.topping@kinandcarta.com>
2025-03-12 10:37:41 +0000

    deleting synapse workspace

--- infrastructure/modules/synapse-workspace-private/key-vault-rbac.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_role_assignment" "synapse_msi_key_vault" {
-  scope                = var.key_vault_id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_synapse_workspace.synapse.identity[0].principal_id
-}
--- infrastructure/modules/synapse-workspace-private/random.tf
+++
@@ -1,9 +0,0 @@
-resource "random_password" "synapse_sql_administrator_password" {
-  length           = 32
-  special          = true
-  override_special = "#&-_+"
-  min_lower        = 2
-  min_upper        = 2
-  min_numeric      = 2
-  min_special      = 2
-}
