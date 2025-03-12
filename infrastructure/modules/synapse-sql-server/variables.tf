035bea6e9bca12b67e23454d22a33364d1c91df1
Chris Topping <chris.topping@kinandcarta.com>
2025-03-12 11:49:35 +0000

    more deletions

--- infrastructure/modules/synapse-sql-server/variables.tf
+++ infrastructure/modules/synapse-sql-server/variables.tf
@@ -46,11 +46,6 @@ variable "sql_server_administrator_username" {
   type        = string
 }
 
-variable "synapse_workspace_id" {
-  description = "The ID of the Synapse Workspace from which to collect diagnostic logs"
-  type        = string
-}
-
 variable "tags" {
   default     = {}
   description = "A collection of tags to assign to taggable resources"
