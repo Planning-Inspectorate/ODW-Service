dbf628576c7711e9f552907dc80ddb31449c09a0
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-03-26 12:00:15 +0000

    feat(infrastructure): add "Storage Blob Data Owner" role to purview instance

--- infrastructure/workload-management.tf
+++ infrastructure/workload-management.tf
@@ -22,6 +22,7 @@ module "synapse_management" {
   location            = module.azure_region.location_cli
   service_name        = local.service_name
 
+  data_lake_account_id                   = module.synapse_data_lake.data_lake_account_id
   deploy_purview                         = true
   devops_agent_subnet_name               = module.synapse_network.devops_agent_subnet_name
   firewall_allowed_ip_addresses          = yamldecode(file(local.firewall_config_file_path))
@@ -66,6 +67,7 @@ module "synapse_management_failover" {
   location            = module.azure_region.paired_location.location_cli
   service_name        = local.service_name
 
+  data_lake_account_id                   = module.synapse_data_lake_failover.data_lake_account_id
   deploy_purview                         = false # Not supported in the UK West region
   devops_agent_subnet_name               = module.synapse_network_failover.devops_agent_subnet_name
   firewall_allowed_ip_addresses          = yamldecode(file(local.firewall_config_file_path))
