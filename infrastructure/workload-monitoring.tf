b9baee09f644a360f6c25e23bd46012910e5b17f
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-04-04 16:57:34 +0100

    Remove spark pool references in the test env

--- infrastructure/workload-monitoring.tf
+++ infrastructure/workload-monitoring.tf
@@ -32,7 +32,6 @@ module "synapse_monitoring" {
   service_bus_namespace_id                 = module.synapse_ingestion.service_bus_namespace_id
   spark_pool_enabled                       = var.spark_pool_enabled
   sql_pool_enabled                         = var.sql_pool_enabled
-  synapse_spark_pool_id                    = module.synapse_workspace_private.synapse_spark_pool_id
   synapse_sql_pool_id                      = module.synapse_workspace_private.synapse_sql_pool_id
   synapse_workspace_id                     = module.synapse_workspace_private.synapse_workspace_id
   synapse_vnet_id                          = module.synapse_network.vnet_id
@@ -60,7 +59,6 @@ module "synapse_monitoring_failover" {
   service_bus_namespace_id                 = module.synapse_ingestion_failover[0].service_bus_namespace_id
   spark_pool_enabled                       = var.spark_pool_enabled
   sql_pool_enabled                         = var.sql_pool_enabled
-  synapse_spark_pool_id                    = module.synapse_workspace_private_failover[0].synapse_spark_pool_id
   synapse_sql_pool_id                      = module.synapse_workspace_private_failover[0].synapse_sql_pool_id
   synapse_workspace_id                     = module.synapse_workspace_private_failover[0].synapse_workspace_id
   synapse_vnet_id                          = module.synapse_network_failover.vnet_id
