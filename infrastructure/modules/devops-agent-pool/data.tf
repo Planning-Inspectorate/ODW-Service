ddaa9c5da232f085827c83ea07c31e69fb6fa280
Medini-Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2025-04-04 11:54:50 +0100

    fix(tooling): rolling back to old azure agent image

--- infrastructure/modules/devops-agent-pool/data.tf
+++ infrastructure/modules/devops-agent-pool/data.tf
@@ -1,7 +1,7 @@
 data "azurerm_image" "azure_agents" {
   count = var.deploy_agent_pool ? 1 : 0
 
-  name_regex          = devops-agents-20241114120119
+  name_regex          = "devops-agents-20241114120119"
   sort_descending     = true
   resource_group_name = azurerm_resource_group.devops_agents.name
 }
