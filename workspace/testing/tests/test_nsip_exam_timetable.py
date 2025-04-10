e0ea734f83c0dc57b40cc9c8cfaf710436b07684
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-04-10 10:22:11 +0100

    Update pool ref to pv pool

--- workspace/testing/tests/test_nsip_exam_timetable.py
+++ workspace/testing/tests/test_nsip_exam_timetable.py
@@ -11,7 +11,7 @@ def test_nsip_exam_timetable_notebook(credential_name, azure_credential, synapse
     notebookname: str = "py_unit_tests_nsip_exam_timetable"
     
     notebook_raw_params = {
-        "sparkPool": "pinssynspodw",
+        "sparkPool": "pinssynspodwpr",
         "notebook": notebookname,
         "sessionOptions": {
             "driverMemory": "28g",
