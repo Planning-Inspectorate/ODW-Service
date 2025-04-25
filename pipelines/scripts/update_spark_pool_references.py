d24638400f95b08230276afdea3237c76181698d
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-04-25 12:37:37 +0100

    Fix logic error

--- pipelines/scripts/update_spark_pool_references.py
+++ pipelines/scripts/update_spark_pool_references.py
@@ -113,15 +113,17 @@ class SparkPoolReferenceUpdater():
                 }
             }
         }
-        compute_options = {
-            "id": new_pool_details["id"],
-            "name": pool_name,
-            "endpoint": f"https://{self._synapse_workspace_name}.azuresynapse.net/livyApi/versions/2019-11-01-preview/sparkPools/{pool_name}",
-            "sparkVersion": new_pool_details["properties"]["sparkVersion"],
-            #"nodeCount": 10,
-            "cores": node_size_details["vCores"],
-            "memory": node_size_details["memory"],
-            "automaticScaleJobs": new_pool_details["properties"]["autoScale"]["enabled"]
+        metadata = {
+            "metadata": {
+                "id": new_pool_details["id"],
+                "name": pool_name,
+                "endpoint": f"https://{self._synapse_workspace_name}.azuresynapse.net/livyApi/versions/2019-11-01-preview/sparkPools/{pool_name}",
+                "sparkVersion": new_pool_details["properties"]["sparkVersion"],
+                #"nodeCount": 10,
+                "cores": node_size_details["vCores"],
+                "memory": node_size_details["memory"],
+                "automaticScaleJobs": new_pool_details["properties"]["autoScale"]["enabled"]
+            }
         }
         logging.info(f"    Replacing references for notebook {notebook['name']}")
         if "bigDataPool" in notebook["properties"]:
@@ -129,7 +131,7 @@ class SparkPoolReferenceUpdater():
                 "referenceName": pool_name
             }
         if "metadata" in notebook["properties"] and "a365ComputeOptions" in notebook["properties"]["metadata"]:
-            properties_to_overwrite["properties"]["metadata"]["a365ComputeOptions"] = compute_options
+            properties_to_overwrite["properties"]["metadata"] = metadata
         return self._merge_dictionaries(notebook, properties_to_overwrite)
 
 
