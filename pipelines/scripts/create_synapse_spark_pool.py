dbc67e774b64bf346795f33d8d633d1ebe36c5f5
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-04-28 11:47:32 +0100

    Add process to create and delete spark pools

---
+++ pipelines/scripts/create_synapse_spark_pool.py
@@ -0,0 +1,12 @@
+from pipelines.scripts.synapse_artifact.synapse_spark_pool_util import SynapseSparkPoolUtil
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-sw", "--synapse_workspace", help="The synapse workspace to check against")
+    parser.add_argument("-pn", "--pool_name", help="The name of the pool to create")
+    args = parser.parse_args()
+    synapse_workspace = args.synapse_workspace
+    pool_name = args.pool_name
+    SynapseSparkPoolUtil(synapse_workspace).create(pool_name)
--- pipelines/scripts/synapse_artifact/synapse_spark_pool_util.py
+++ pipelines/scripts/synapse_artifact/synapse_spark_pool_util.py
@@ -1,5 +1,10 @@
+from pipelines.scripts.util import Util
 from pipelines.scripts.synapse_artifact.synapse_artifact_util import SynapseArtifactUtil
 from typing import List, Dict, Any
+import requests
+import json
+import time
+import logging
 
 
 class SynapseSparkPoolUtil(SynapseArtifactUtil):
@@ -25,6 +30,92 @@ class SynapseSparkPoolUtil(SynapseArtifactUtil):
             response = self._web_request(next_link,).json()
             all_notebooks.extend(response["value"])
         return all_notebooks
+    
+    def create(self, pool_name: str):
+        """
+            Create a spark pool with default settings with the given name
+        """
+        synapse_workspace_id = self._web_request(f"{self.synapse_endpoint}/workspace?api-version=2020-12-01").json()["id"]
+        endpoint = f"https://management.azure.com{synapse_workspace_id}/bigDataPools/{pool_name}?api-version=2021-06-01"
+
+        payload = {
+            "properties": {
+                "creationDate": "0001-01-01T00:00:00",
+                "sparkVersion": "3.4",
+                "nodeCount": 0,
+                "nodeSize": "Small",
+                "nodeSizeFamily": "MemoryOptimized",
+                "autoScale": {
+                    "enabled": True,
+                    "minNodeCount": 3,
+                    "maxNodeCount": 12
+                },
+                "autoPause": {
+                    "enabled": True,
+                    "delayInMinutes": 60
+                },
+                "isComputeIsolationEnabled": False,
+                "cacheSize": 0,
+                "dynamicExecutorAllocation": {
+                    "enabled": False
+                },
+                "isAutotuneEnabled": True,
+                "provisioningState": "Succeeded"
+            },
+            "location": "UK South",
+            "tags": dict()
+        }
+        access_token = json.loads(Util.run_az_cli_command(["az", "account", "get-access-token", "--output", "json"]))["accessToken"]
+
+        resp = requests.put(
+            endpoint,
+            json=payload,
+            headers={'Authorization': f'Bearer {access_token}'}
+        )
+        max_timeout = 30
+        current_timeout = 0
+        provisioned = False
+        # Wait for the pool to be created before continuing
+        pools = self.get_all()
+        wait_interval = 1
+        while ((not provisioned) or pool_name not in {pool["name"] for pool in pools}) and current_timeout < max_timeout:
+            logging.info(f"    Pool is being created - waiting {wait_interval} seconds before checking again...")
+            time.sleep(wait_interval)
+            current_timeout += wait_interval
+            pools = self.get_all()
+            target_pool = [x for x in pools if x["name"] == pool_name]
+            if target_pool:
+                logging.info(f"    The pool has been created, its provisioning state is '{target_pool[0]['properties']['provisioningState']}'")
+                provisioned = target_pool[0]["properties"]["provisioningState"] == "Succeeded"
+        if current_timeout >= max_timeout:
+            raise RuntimeError(
+                f"Exceeded max wait time when creating the spark pool '{pool_name}'. Http response was:\n {json.dumps(resp, indent=4)}"
+            )
+
+    def delete(self, pool_name: str):
+        """
+            Delete the spark pool with the given name
+        """
+        synapse_workspace_id = self._web_request(f"{self.synapse_endpoint}/workspace?api-version=2020-12-01").json()["id"]
+        endpoint = f"https://management.azure.com{synapse_workspace_id}/bigDataPools/{pool_name}?api-version=2021-06-01"
+        access_token = json.loads(Util.run_az_cli_command(["az", "account", "get-access-token", "--output", "json"]))["accessToken"]
+
+        resp = requests.delete(
+            endpoint,
+            headers={'Authorization': f'Bearer {access_token}'}
+        )
+        max_timeout = 30
+        current_timeout = 0
+        wait_interval = 1
+        # Wait for the pool to be destroyed before continuing
+        while pool_name in {pool["name"] for pool in self.get_all()} and current_timeout < max_timeout:
+            logging.info(f"    Pool is being deleted - waiting {wait_interval} seconds before checking again...")
+            time.sleep(wait_interval)
+            current_timeout += wait_interval
+        if current_timeout >= max_timeout:
+            raise RuntimeError(
+                f"Exceeded max wait time when deleting the spark pool '{pool_name}'. Http response was:\n {json.dumps(resp.json(), indent=4)}"
+            )
 
     def get_uncomparable_attributes(self) -> List[str]:
         return []
