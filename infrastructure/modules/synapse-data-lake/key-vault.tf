a30b2207fe9eba56ea13e60b1c57fa15f69d426e
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-05-12 12:06:07 +0100

    Add conditional logic for key vault creation:

--- infrastructure/modules/synapse-data-lake/key-vault.tf
+++ infrastructure/modules/synapse-data-lake/key-vault.tf
@@ -1,6 +1,7 @@
 resource "azurerm_key_vault" "synapse" {
   #checkov:skip=CKV2_AZURE_32: Managed Private Endpoint is enabled for the Key Vault
   #checkov:skip=CKV_AZURE_189: Firewall is enabled in the network_acls block
+  count                      = var.external_resource_links_enabled ? 1 : 0
   name                       = replace("pins-kv-synw-${local.resource_suffix}", "-", "")
   resource_group_name        = var.resource_group_name
   location                   = var.location
