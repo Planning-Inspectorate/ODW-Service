fde39ea961571f9b4f0a91ca668aaa2d43666298
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-05-14 16:33:52 +0100

    Remove unused import

--- pipelines/scripts/private_endpoint/synapse_private_endpoint_manager.py
+++ pipelines/scripts/private_endpoint/synapse_private_endpoint_manager.py
@@ -1,5 +1,4 @@
 from pipelines.scripts.private_endpoint.private_endpoint_manager import PrivateEndpointManager
-from pipelines.scripts.util import Util
 from azure.mgmt.synapse import SynapseManagementClient
 from typing import List, Type
 
