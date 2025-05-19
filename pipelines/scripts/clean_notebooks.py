40969a49b596191ed37a3b14fa6f945142152c9c
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-05-19 14:16:43 +0100

    Improve logging

--- pipelines/scripts/clean_notebooks.py
+++ pipelines/scripts/clean_notebooks.py
@@ -13,20 +13,20 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 
-def clean_notebook(notebook: Dict[str, Any]) -> bool:
+def clean_notebook(notebook: Dict[str, Any]) -> int:
     """
         Clean the given synapse notebook
 
         :param notebook: The notebook json to clean
         :return: `True` if the notebook had any of its properties cleaned, `False` otherwise
     """
-    modified_attribute = False
+    modified_count = 0
     for cell in notebook["properties"]["cells"]:
         if "execution_count" in cell:
             if cell["execution_count"] is not None:
                 cell["execution_count"] = None
-                modified_attribute = True
-    return modified_attribute
+                modified_count += 1
+    return modified_count
 
 
 def clean_notebooks(notebooks_to_clean: List[str]):
@@ -38,9 +38,9 @@ def clean_notebooks(notebooks_to_clean: List[str]):
     for notebook_path in notebooks_to_clean:
         logging.info(f"Cleaning notebook '{notebook_path}'")
         notebook = json.load(open(notebook_path))
-        cleaned_notebook = clean_notebook(notebook)
-        if cleaned_notebook:
-            logging.info("    Some properties of this notebook have been cleaned")
+        cleaned_cell_count = clean_notebook(notebook)
+        if cleaned_cell_count > 0:
+            logging.info(f"    Some properties of this notebook have been cleaned. Modified {cleaned_cell_count} cells")
         with open(notebook_path, "w") as f:
             json.dump(notebook, f, indent="\t", ensure_ascii=False)
 
