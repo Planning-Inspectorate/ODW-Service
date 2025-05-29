d91432369ad239e7ee651063c2642c13244c2252
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-05-29 16:25:52 +0100

    Move resource reference to dedicated file

---
+++ infrastructure/external-resources.tf
@@ -0,0 +1,7 @@
+data "azurerm_servicebus_namespace" "odt_appeals_backoffice_sb" {
+  count               = var.odt_appeals_back_office.service_bus_enabled && var.external_resource_links_enabled ? 1 : 0
+  name                = var.odt_appeals_back_office.service_bus_name
+  resource_group_name = var.odt_appeals_back_office.resource_group_name
+
+  provider = azurerm.odt
+}
