e8056296873c144192582c42f4b23d30c287aad2
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-06-02 09:32:44 +0100

    Add refactoring blocks

--- infrastructure/workload-agent-pool.tf
+++ infrastructure/workload-agent-pool.tf
@@ -34,3 +34,18 @@ module "devops_agent_pool_failover" {
 
   tags = local.tags
 }
+
+moved {
+  from = module.devops_agent_pool_failover.azurerm_linux_virtual_machine_scale_set.devops_agent_pool[0]
+  to   = module.devops_agent_pool_failover[0].azurerm_linux_virtual_machine_scale_set.devops_agent_pool[0]
+}
+
+moved {
+  from = module.devops_agent_pool_failover.azurerm_resource_group.devops_agents
+  to   = module.devops_agent_pool_failover[0].azurerm_resource_group.devops_agents
+}
+
+moved {
+  from = module.devops_agent_pool_failover.random_password.devops_agent_password[0]
+  to   = module.devops_agent_pool_failover[0].random_password.devops_agent_password[0]
+}
