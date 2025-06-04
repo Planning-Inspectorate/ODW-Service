0493c036486e5634d325fc0ee28e96c12f3cdb77
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-06-04 13:29:05 +0100

    Remove redundant resource

--- infrastructure/modules/synapse-management/key-vault-rbac.tf
+++ infrastructure/modules/synapse-management/key-vault-rbac.tf
@@ -13,11 +13,3 @@ resource "azurerm_role_assignment" "key_vault_terraform" {
   role_definition_name = "Key Vault Administrator"
   principal_id         = data.azurerm_client_config.current.object_id
 }
-
-resource "azurerm_role_assignment" "purview_msi_key_vault" {
-  count = var.deploy_purview ? 1 : 0
-
-  scope                = azurerm_key_vault.management.id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_purview_account.management[0].identity[0].principal_id
-}
