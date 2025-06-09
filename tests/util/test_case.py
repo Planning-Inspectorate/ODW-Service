6f7d6cdcd72a0ebc194d10775d1a3c5a491f1bde
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-06-09 14:02:52 +0100

    Add generic test case

---
+++ tests/util/test_case.py
@@ -0,0 +1,15 @@
+class TestCase():
+    """
+        Generic test case class
+    """
+    def module_setup(self):
+        """
+            Initialise any common dependencies for the whole test case before testing begins
+        """
+        pass
+
+    def module_teardown(self):
+        """
+            Remove any created components during testing after testing has completed
+        """
+        pass
