3053e6377f089d3f106d26cc9499c95278ad95ba
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-06-10 16:14:05 +0100

    Update test fixtures

--- tests/unit_test/test_azure_functions.py
+++ tests/unit_test/test_azure_functions.py
@@ -11,24 +11,33 @@ class TestAzureFunctions(TestCase):
     KEY_VAULT_CLIENT = SecretClient(vault_url=f"https://pinskvsynwodw{TEST_CONFIG['ENV'].lower()}uks.vault.azure.net", credential=AzureCliCredential())
 
     @pytest.mark.parametrize(
-        "test_parameters",
+        "function_name,url_parameters",
         [
-            {
-                "function_name": "getDaRT",
-                "url_parameters": "&caseReference=&applicationReference="
-            },
-            {
-                "function_name": "gettimesheets",
-                "url_parameters": "&searchCriteria="
-            },
+            ("folder", ""),
+            ("nsipdocument", ""),
+            ("nsipexamtimetable", ""),
+            ("nsipproject", ""),
+            ("nsipprojectupdate", ""),
+            ("nsiprepresentation", ""),
+            ("s51advice", ""),
+            ("nsipsubscription", ""),
+            ("serviceuser", ""),
+            ("appealdocument", ""),
+            ("appealhas", ""),
+            ("appealevent", ""),
+            ("appealserviceuser", ""),
+            ("getDaRT", "&caseReference=&applicationReference="),
+            ("appeals78", ""),
+            ("appealrepresentation", ""),
+            ("gettimesheets", "&searchCriteria="),
+            ("appealeventestimate", ""),
+            ("serviceuser", "")
         ]
     )
-    def test_azure_function_calls(self, test_parameters: str):
-        function_name = test_parameters.get("function_name")
-        url_parameters = test_parameters.get("url_parameters", dict())
+    def test_azure_function_calls(self, function_name: str, url_parameters: str):
         function_secret_name = f"function-url-{function_name.replace('-', '')}"
         function_url = f"{self.KEY_VAULT_CLIENT.get_secret(function_secret_name).value}{url_parameters}"
         response = requests.get(function_url)
-        assert response.status_code == 200, f"Expected the status code to be 200, but was '{response.status_code}'"
+        assert response.status_code == 200, f"Expected the status code to be 200 for function url '{function_url}', but was '{response.status_code}'"
         parsed_json = json.loads(response.text)
-        assert len(parsed_json) > 0, "Expected the JSON response to have some content, but it was empty"
+        assert len(parsed_json) > 0, f"Expected the JSON response to have some content for function url '{function_url}', but it was empty"
