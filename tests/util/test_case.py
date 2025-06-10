613aecb52a47805adaf46d89f8c03a7ab5b2fd6f
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-06-10 11:42:16 +0100

    Add fixture calls

--- tests/util/test_case.py
+++ tests/util/test_case.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 class TestCase():
     """
         Generic test case class
@@ -8,8 +11,24 @@ class TestCase():
         """
         pass
 
+    @pytest.fixture(scope="module", autouse=True)
     def module_teardown(self):
         """
             Remove any created components during testing after testing has completed
         """
         pass
+
+    @pytest.fixture(scope="module", autouse=True)
+    def _run_module_setup(self):
+        """
+            Call the module setup function when a test case starts
+        """
+        self.module_setup()
+
+    @pytest.fixture(scope="module", autouse=True)
+    def _run_module_teardown(self):
+        """
+            Call the module teardown function when a test case ends
+        """
+        yield
+        self.module_teardown()
