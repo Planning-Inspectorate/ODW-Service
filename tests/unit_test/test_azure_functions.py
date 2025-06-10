0df5d98eb2a095e3c431ec7af0d9ff1e15d5b913
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-06-10 13:46:43 +0100

    Remove notebook call

--- tests/unit_test/test_azure_functions.py
+++ tests/unit_test/test_azure_functions.py
@@ -1,8 +1,15 @@
 from tests.util.notebook_run_test_case import NotebookRunTestCase
+from azure.identity import AzureCliCredential
+from azure.keyvault.secrets import SecretClient
+from tests.util.test_case import TestCase
+import requests
 import pytest
+import json
 
 
-class TestAzureFunctions(NotebookRunTestCase):
+class TestAzureFunctions(TestCase):
+    CLIENT = SecretClient(vault_url="https://pinskvsynwodwdevuks.vault.azure.net", credential=AzureCliCredential())
+
     @pytest.mark.parametrize(
         "test_parameters",
         [
@@ -17,4 +24,11 @@ class TestAzureFunctions(NotebookRunTestCase):
         ]
     )
     def test_azure_function_calls(self, test_parameters: str):
-        self.run_notebook("py_unit_tests_azure_functions", test_parameters)
+        function_name = test_parameters.get("function_name")
+        url_parameters = test_parameters.get("url_parameters", dict())
+        function_secret_name = f"function-url-{function_name.replace('-', '')}"
+        function_url = f"{self.CLIENT.get_secret(function_secret_name)}{url_parameters}"
+        response = requests.get(function_url)
+        assert response.status_code == 200, f"Expected the status code to be 200, but was '{response.status_code}'"
+        parsed_json = json.loads(response.text)
+        assert len(parsed_json) > 0, "Expected the JSON response to have some content, but it was empty"
