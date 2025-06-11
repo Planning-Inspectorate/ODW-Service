8220838522d794d3259dd03a990a59a90280eb9c
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-06-11 10:30:35 +0100

    Improve error handling

--- tests/unit_test/test_py_utils_get_storage_account.py
+++ tests/unit_test/test_py_utils_get_storage_account.py
@@ -6,5 +6,5 @@ class TestPyUtilsGetStorageAccount(NotebookRunTestCase):
     def test_py_utils_get_storage_account(self):
         return_value = self.run_notebook("py_utils_get_storage_account", dict())
         expected_exit_value = f"{TEST_CONFIG['DATA_LAKE_STORAGE']}.dfs.core.windows.net/"
-        exit_value = return_value["result"]["exitValue"]
+        exit_value = return_value["result"].get("exitValue", None)
         assert exit_value == expected_exit_value
