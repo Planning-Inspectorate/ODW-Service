c4cee44d9059103f926aa300ee885001994cef28
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-06-11 15:02:27 +0100

    Rename purview account. Create purview account in the dev subscription

--- infrastructure/modules/synapse-management/purview.tf
+++ infrastructure/modules/synapse-management/purview.tf
@@ -1,7 +1,7 @@
 resource "azurerm_purview_account" "management" {
   count = var.deploy_purview ? 1 : 0
 
-  name                        = "pins-pview-${local.resource_suffix}"
+  name                        = "pins-pview-${var.service_name}"
   resource_group_name         = var.resource_group_name
   location                    = var.location
   managed_resource_group_name = "${var.resource_group_name}-purview-managed"
