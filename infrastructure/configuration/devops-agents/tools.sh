ea4b422938034f735aea0487ea4ca4bca4b832cb
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-06-13 10:26:38 +0100

    chore(infrastructure): upgrade to terraform 1.12.2

--- infrastructure/configuration/devops-agents/tools.sh
+++ infrastructure/configuration/devops-agents/tools.sh
@@ -50,10 +50,10 @@ sudo apt install -y --no-install-recommends \
   python3.7-distutils \
   python3-pip
 
-# Terraform 1.11.4
+# Terraform
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get install -y terraform=1.11.4-1
+sudo apt-get install -y terraform=1.12.2-1
 
 # Checkov
 python3.7 -m pip install --force-reinstall packaging==21
