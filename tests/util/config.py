9ae077fa1cc97e5c2f27c0cf55d983326adc9509
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-06-13 13:26:46 +0100

    Add smoke tests

--- tests/util/config.py
+++ tests/util/config.py
@@ -11,6 +11,8 @@ load_dotenv(verbose=True)
 TEST_CONFIG = {
     k: os.environ.get(k, None)
     for k in [
-        "ENV"
+        "ENV",
+        "SUBSCRIPTION_ID",
+        "PURVIEW_ID"
     ]
 }
