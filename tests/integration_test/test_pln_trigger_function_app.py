36998cd67b2079c399fb83ddd2c3b0f99e51b642
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-06-18 12:23:48 +0100

    Add integration tests

---
+++ tests/integration_test/test_pln_trigger_function_app.py
@@ -0,0 +1,39 @@
+from tests.util.pipeline_run_test_case import PipelineRunTestCase
+import pytest
+import json
+
+class TestPlnTriggerFunctionApp(PipelineRunTestCase):
+    @pytest.mark.parametrize(
+        "function_name",
+        [
+            "folder",         
+            # "nsipdocument",
+            # "nsipexamtimetable",
+            # "nsipproject",
+            # "nsipprojectupdate",
+            # "nsiprepresentation",
+            # "s51advice",
+            # "nsipsubscription",
+            # "serviceuser",
+            # "appealdocument",
+            # "appealhas",
+            # "appealevent",
+            # "appealserviceuser",
+            # "getDaRT",
+            # "appeals78",
+            # "appealrepresentation",
+            # "gettimesheets",
+            # "appealeventestimate",
+            # "serviceuser"
+
+        ]
+    )
+    def test_pln_trigger_function_app(self, function_name: str):
+        pipeline_result = self.run_pipeline(
+            "pln_trigger_function_app",
+            {
+                "function_name": function_name
+            }
+        )
+        pipeline_return_value = pipeline_result["pipelineReturnValue"]
+        assert "Messages" in pipeline_return_value and isinstance(pipeline_return_value["Messages"], int) and pipeline_return_value["Messages"] >= 0
