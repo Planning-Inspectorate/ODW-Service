a99c96993224e22a8d9a9eab713adc8de1fd3778
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-06-19 16:04:30 +0100

    Update class name

--- tests/unit_test/test_unit_py_utils_get_storage_account.py
+++ tests/unit_test/test_unit_py_utils_get_storage_account.py
@@ -2,7 +2,7 @@ from tests.util.config import TEST_CONFIG
 from tests.util.notebook_run_test_case import NotebookRunTestCase
 
 
-class TestPyUtilsGetStorageAccount(NotebookRunTestCase):
+class TestUnitPyUtilsGetStorageAccount(NotebookRunTestCase):
     def test_py_utils_get_storage_account(self):
         return_value = self.run_notebook("py_utils_get_storage_account", dict())
         expected_exit_value = f"{TEST_CONFIG['DATA_LAKE_STORAGE']}.dfs.core.windows.net/"
