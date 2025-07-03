f72ed7559bcab90364c1d453f9a756a0402e2e7e
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-07-03 16:50:36 +0100

    Rearrange order of logging statements to make archival process more legible

--- pipelines/scripts/archive_artifacts.py
+++ pipelines/scripts/archive_artifacts.py
@@ -260,7 +260,6 @@ class ArtifactArchiver():
             Identify artifacts that can be archived or deleted, and then archive/delete them
         """
         logging.info(f"Identifying the dependencies of the root artifacts {self.ROOT_ARTIFACTS}")
-        logging.info(f"A total of {len(self.ALL_ARTIFACT_NAMES)} artifacts have been discovered")
         # Get all artifacts that are essential for the ODW (i.e. all components related to the root artifacts)
         dependencies = set(self.ROOT_ARTIFACTS)
         for artifact in self.ROOT_ARTIFACTS:
