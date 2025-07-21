5a933006674a097c80792ad4f0e828abd1b9b282
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-07-21 11:38:18 +0100

    Update notebook wrapping for the smoke test

--- tests/smoke_test/test_smoke_synapse_connectivity.py
+++ tests/smoke_test/test_smoke_synapse_connectivity.py
@@ -1,5 +1,4 @@
 from tests.util.notebook_run_test_case import NotebookRunTestCase
-import tests.util.constants as constants
 import json
 
 
@@ -11,4 +10,7 @@ class TestSmokeSynapseConnectivity(NotebookRunTestCase):
         notebook_name = "test_smoke_py_connectivity"
         notebook_parameters = dict()
 
-        self.run_notebook(notebook_name, notebook_parameters)
+        notebook_run_result = self.run_notebook(notebook_name, notebook_parameters)
+        exit_value = notebook_run_result["result"]["exitValue"]
+        failing_tests = json.loads(exit_value)
+        assert not failing_tests, f"The following tests failed in the test_smoke_py_connectivity notebook : {json.dumps(failing_tests, indent=4)}"
