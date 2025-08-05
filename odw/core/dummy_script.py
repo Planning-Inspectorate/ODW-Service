b0e5e4a4be3a93c72a5e66146d704e7e8b1c61b2
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-08-05 15:58:06 +0100

    Remove dummy files used for testing

--- odw/core/dummy_script.py
+++
@@ -1,5 +0,0 @@
-from odw.core.nested_dummy.inner_module import inner_function
-
-
-def a_test_function():
-    return inner_function()
--- odw/core/nested_dummy/inner_module.py
+++
@@ -1,2 +0,0 @@
-def inner_function():
-    return "Hello world"
