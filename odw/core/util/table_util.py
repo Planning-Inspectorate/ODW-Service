380512d128e3364732a783da3f9c62810fa63280
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-08-11 17:28:06 +0100

    Fix logic errors. Add full test coverage

--- odw/core/util/table_util.py
+++ odw/core/util/table_util.py
@@ -11,8 +11,9 @@ class TableUtil():
     """
         Utility class for interacting with tables
     """
+    @classmethod
     @LoggingUtil.logging_to_appins
-    def delete_table(db_name: str, table_name: str):
+    def delete_table(cls, db_name: str, table_name: str):
         """
             Delete the given table in the given database. This should be used for tables that do not use
             delta as the underlying storag mechanism
@@ -27,20 +28,22 @@ class TableUtil():
             :param table_name: The name of the table to delete
         """
         spark = SparkSession.builder.getOrCreate()
-        if spark._jsparkSession.catalog().tableExists(db_name, table_name):
-            table_details = spark.sql(f"DESCRIBE DETAIL {db_name}.{table_name}").toPandas()
-            if len(table_details) > 1:
+        if spark.catalog.tableExists(table_name, db_name):
+            table_details_query = spark.sql(f"DESCRIBE DETAIL {db_name}.{table_name}")
+            num_tables = table_details_query.count()
+            if num_tables > 1:
                 raise RuntimeError("too many locations associated with the table!")
             else:
-                loc = table_details["location"][0]
+                loc = table_details_query.select("location").first().location
                 mssparkutils.fs.rm(loc, True)
                 spark.sql(f"DROP TABLE IF EXISTS {db_name}.{table_name}")
                 LoggingUtil().log_info(f"Dropped table {db_name}.{table_name}")
         else:
             LoggingUtil().log_info("Table does not exist")
 
+    @classmethod
     @LoggingUtil.logging_to_appins
-    def delete_table_contents(db_name: str, table_name: str):
+    def delete_table_contents(cls, db_name: str, table_name: str):
         """
             Delete the content from the given table in the given database. This should be used for
             tables that use delta format as the underlying storage mechanism
@@ -49,9 +52,10 @@ class TableUtil():
             :param table_name: The name of the table to delete
         """
         spark = SparkSession.builder.getOrCreate()
-        if spark._jsparkSession.catalog().tableExists(db_name, table_name):
-            table_details = spark.sql(f"DESCRIBE DETAIL {db_name}.{table_name}").toPandas()
-            if len(table_details) > 1:
+        if spark.catalog.tableExists(table_name, db_name):
+            table_details_query = spark.sql(f"DESCRIBE DETAIL {db_name}.{table_name}")
+            num_tables = table_details_query.count()
+            if num_tables > 1:
                 raise RuntimeError("too many locations associated with the table!")
             else:
                 spark.sql(f"DELETE FROM {db_name}.{table_name}")
